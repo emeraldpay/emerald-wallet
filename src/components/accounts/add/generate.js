219761a7f487db1c316b4912e59a7a310fa1feb4
Elaine Ou <elaine.ou@gmail.com>
2017-03-17 20:49:29 -0700

    accounts/add/generate.js: update generate form with material-ui

--- src/components/accounts/add/generate.js
+++ src/components/accounts/add/generate.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm } from 'redux-form/immutable'
+import TextField from 'material-ui/TextField'
 
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import FontIcon from 'material-ui/FontIcon'
-import {List, ListItem} from 'material-ui/List'
-import Subheader from 'material-ui/Subheader'
 
 import { cardSpace } from 'lib/styles'
 import { Row, Col } from 'react-flexbox-grid/lib/index'
@@ -15,31 +14,65 @@ import { gotoScreen } from 'store/screenActions'
 import { positive, number, required, address } from 'lib/validators'
 import log from 'loglevel'
 
-class GenerateAccountForm extends Component {
-    render() {
-        const { handleSubmit } = this.props;
-        return (
-            <form onSubmit={handleSubmit}>
-                    <div>
-              <label htmlFor="name">Account Name (optional)</label>
-              <Field name="name" component="input" type="text"/>
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <Field name="password" component="input" type="password"/>
-            </div>
-            <div>
-              <label htmlFor="password-conf">Re-Enter Password</label>
-              <Field name="password-conf" component="input" type="password"/>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-            );
-    }
+const renderTextField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <TextField {...input} type={type}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
+
+const Render = ({handleSubmit, pristine, reset, submitting, cancel}) => {
+        
+    return (
+        <Card style={cardSpace}>
+            <CardHeader
+                title='Generate Wallet'
+                actAsExpander={false}
+                showExpandableButton={false}
+            />
+
+            <CardText expandable={false}>
+                <form onSubmit={handleSubmit}>
+                    <Field name="name" component={renderTextField} type="text" label="Account Name (optional)"/>
+                    <Field name="password" component={renderTextField} type="password" label="Please Enter a Strong Password"/>
+                    <Field name="password-conf" component={renderTextField} type="password" label="Re-Enter Password"/>
+                    <FlatButton label="Submit" disabled={pristine || submitting}/>
+                    <FlatButton label="Clear Values" disabled={pristine || submitting} onClick={reset}/>
+                </form>
+            </CardText>
+
+            <CardActions>
+                <FlatButton label="Cancel"
+                            onClick={cancel}
+                            icon={<FontIcon className="fa fa-ban" />}/>
+            </CardActions>
+        </Card>
+        );
 }
 
 const GenerateAccountForm = reduxForm({
-    form: 'generate'
-})(GenerateAccountForm);
+    form: 'generate',
+    fields: []
+})(Render);
 
-export default GenerateAccountForm;
+const GenerateAccount = connect(
+    (state, ownProps) => {
+        return {
+            initialValues: {
+            }
+        }
+    },
+    (dispatch, ownProps) => {
+        return {
+            cancel: () => {
+                dispatch(gotoScreen('home'))
+            }
+        }
+    }
+)(GenerateAccountForm);
+
+export default GenerateAccount;
