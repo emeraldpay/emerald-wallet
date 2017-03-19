a892924788f4d78d271c7e27838b85d3970d5785
Elaine Ou <elaine.ou@gmail.com>
2017-03-19 00:19:59 -0700

    accounts/add/generate.js: generate account with password validation

--- src/components/accounts/add/generate.js
+++ src/components/accounts/add/generate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form/immutable'
+import { Field, reduxForm } from 'redux-form'
 import TextField from 'material-ui/TextField'
 
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
@@ -11,21 +11,29 @@ import { cardSpace } from 'lib/styles'
 import { Row, Col } from 'react-flexbox-grid/lib/index'
 
 import { gotoScreen } from 'store/screenActions'
-import { positive, number, required, address } from 'lib/validators'
+import { loadAccountsList, createAccount } from 'store/accountActions'
+import { required, password, passwordMatch } from 'lib/validators'
 import log from 'loglevel'
+
+const validate = values => {
+    const errors = {};
+    errors.password = password(values.password);
+    errors.passwordConfirm = passwordMatch(values.password, values);
+    return errors;
+}
+
 
 const renderTextField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
     <label>{label}</label>
     <div>
-      <TextField {...input} type={type}/>
-      {touched && error && <span>{error}</span>}
+      <TextField {...input} type={type} errorText={touched && error} />
     </div>
   </div>
 )
 
 
-const Render = ({handleSubmit, pristine, reset, submitting, cancel}) => {
+const Render = ({handleSubmit, invalid, pristine, reset, submitting, cancel}) => {
         
     return (
         <Card style={cardSpace}>
@@ -37,11 +45,25 @@ const Render = ({handleSubmit, pristine, reset, submitting, cancel}) => {
 
             <CardText expandable={false}>
                 <form onSubmit={handleSubmit}>
-                    <Field name="name" component={renderTextField} type="text" label="Account Name (optional)"/>
-                    <Field name="password" component={renderTextField} type="password" label="Please Enter a Strong Password"/>
-                    <Field name="password-conf" component={renderTextField} type="password" label="Re-Enter Password"/>
-                    <FlatButton label="Submit" disabled={pristine || submitting}/>
-                    <FlatButton label="Clear Values" disabled={pristine || submitting} onClick={reset}/>
+                    <Field  name="name" 
+                            component={renderTextField} 
+                            type="text" 
+                            label="Account Name (optional)" />
+                    <Field  name="password" 
+                            component={renderTextField} 
+                            type="password" 
+                            label="Please Enter a Strong Password" 
+                            validate={[ required, password ]} />
+                    <Field  name="passwordConfirm" 
+                            component={renderTextField} 
+                            type="password" 
+                            label="Re-Enter Password" 
+                            validate={ passwordMatch } />
+                    <FlatButton label="Submit" type="submit"
+                                disabled={pristine || submitting || invalid } />
+                    <FlatButton label="Clear Values" 
+                                disabled={pristine || submitting} 
+                                onClick={reset} />
                 </form>
             </CardText>
 
@@ -56,7 +78,8 @@ const Render = ({handleSubmit, pristine, reset, submitting, cancel}) => {
 
 const GenerateAccountForm = reduxForm({
     form: 'generate',
-    fields: []
+    fields: ['name', 'password', 'passwordConfirm'],
+    validate
 })(Render);
 
 const GenerateAccount = connect(
@@ -68,6 +91,11 @@ const GenerateAccount = connect(
     },
     (dispatch, ownProps) => {
         return {
+            onSubmit: data => {                
+                dispatch(createAccount(data.name, data.password));
+                // display account ID, click DONE to go home
+            }, 
+            // when done, dispatch(loadAccountsList());
             cancel: () => {
                 dispatch(gotoScreen('home'))
             }
--- src/lib/validators.js
+++ src/lib/validators.js
@@ -13,3 +13,11 @@ export function positive(value) {
 export function address(value) {
     return /^0x[a-fA-F0-9]{40}$/.test(value) ? undefined : "Not an address"
 }
+
+export function password(value) {
+    return ((value) && (value.length >= 8)) ? undefined : "Please enter at least 8 characters"
+}
+
+export function passwordMatch(value, allValues) {
+    return value === allValues.password ? undefined : "Passwords must match"
+}
