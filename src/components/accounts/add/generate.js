c2e3bbe3fc268cc4c97d308cde42ba914d5f97cb
Elaine Ou <elaine.ou@gmail.com>
2017-03-19 19:08:45 -0700

    accounts/add/generate.js: Display new account

--- src/components/accounts/add/generate.js
+++ src/components/accounts/add/generate.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm, reset } from 'redux-form'
 import TextField from 'material-ui/TextField'
-
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import FontIcon from 'material-ui/FontIcon'
@@ -10,10 +9,12 @@ import FontIcon from 'material-ui/FontIcon'
 import { cardSpace } from 'lib/styles'
 import { Row, Col } from 'react-flexbox-grid/lib/index'
 
+import Immutable from 'immutable'
 import { gotoScreen } from 'store/screenActions'
 import { loadAccountsList, createAccount } from 'store/accountActions'
 import { required, password, passwordMatch } from 'lib/validators'
 import log from 'loglevel'
+import AccountShow from '../show'
 
 const validate = values => {
     const errors = {};
@@ -33,8 +34,9 @@ const renderTextField = ({ input, label, type, meta: { touched, error } }) => (
 )
 
 
-const Render = ({handleSubmit, invalid, pristine, reset, submitting, cancel}) => {
-        
+const Render = ({account, submitSucceeded, handleSubmit, invalid, pristine, reset, submitting, cancel}) => {
+    var shit = "lalala"
+
     return (
         <Card style={cardSpace}>
             <CardHeader
@@ -43,7 +45,7 @@ const Render = ({handleSubmit, invalid, pristine, reset, submitting, cancel}) =>
                 showExpandableButton={false}
             />
 
-            <CardText expandable={false}>
+            <CardText expandable={submitSucceeded}>
                 <form onSubmit={handleSubmit}>
                     <Field  name="name" 
                             component={renderTextField} 
@@ -66,7 +68,12 @@ const Render = ({handleSubmit, invalid, pristine, reset, submitting, cancel}) =>
                                 onClick={reset} />
                 </form>
             </CardText>
-
+            <CardText expandable={!submitSucceeded}>
+                 <AccountShow key={(account===undefined) ? undefined : account.get('id')} account={account}/>
+                 <FlatButton label="Done"
+                            onClick={cancel}
+                            icon={<FontIcon className="fa fa-home" />}/>
+            </CardText>
             <CardActions>
                 <FlatButton label="Cancel"
                             onClick={cancel}
@@ -85,17 +92,24 @@ const GenerateAccountForm = reduxForm({
 const GenerateAccount = connect(
     (state, ownProps) => {
         return {
-            initialValues: {
-            }
+            account: state.accounts.get('accounts', Immutable.List()).last()
         }
     },
     (dispatch, ownProps) => {
         return {
             onSubmit: data => {                
-                dispatch(createAccount(data.name, data.password));
-                // display account ID, click DONE to go home
+                return new Promise((resolve, reject) => {
+                    dispatch(createAccount(data.name, data.password))
+                        .then((response) => {
+                            console.log(response);
+                            resolve(response);
+                        });
+                    });
             }, 
-            // when done, dispatch(loadAccountsList());
+            onSubmitSuccess: result => {
+                //display result
+                dispatch(reset('generate'))
+            },
             cancel: () => {
                 dispatch(gotoScreen('home'))
             }
