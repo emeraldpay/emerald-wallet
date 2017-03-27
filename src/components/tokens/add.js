909ee8464bd7c2cc8cf59c71d7ab9241bdfc842a
Elaine Ou <elaine.ou@gmail.com>
2017-03-26 22:01:17 -0700

    components/tokens/add: remove decimal, symbol

--- src/components/tokens/add.js
+++ src/components/tokens/add.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Field, reduxForm, reset } from 'redux-form'
-import { renderTextField } from 'lib/formFields'
+import { renderTextField } from 'elements/formFields'
 import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import FontIcon from 'material-ui/FontIcon'
-import { GridList } from 'material-ui/GridList'
+import { Row, Col } from 'react-flexbox-grid/lib/index'
 
 import { cardSpace } from 'lib/styles'
 
 import Immutable from 'immutable'
 import { gotoScreen } from 'store/screenActions'
 import { addToken } from 'store/tokenActions'
-import { required, positive, number, address } from 'lib/validators'
+import { required, address } from 'lib/validators'
 import log from 'loglevel'
 import Token from './token'
 
@@ -45,16 +45,6 @@ const Render = ({token, submitSucceeded, handleSubmit, invalid, pristine, reset,
                             type="text" 
                             label="Token Name"
                             validate={required} />
-                    <Field  name="symbol" 
-                            component={renderTextField} 
-                            type="text" 
-                            label="Token Symbol"
-                            validate={required} />
-                    <Field  name="decimal" 
-                            component={renderTextField} 
-                            type="number" 
-                            label="Decimal Places of Smallest Unit" 
-                            validate={[number, positive, required]} />
                     <FlatButton label="Submit" type="submit"
                                 disabled={pristine || submitting || invalid } />
                     <FlatButton label="Clear Values" 
@@ -63,10 +53,12 @@ const Render = ({token, submitSucceeded, handleSubmit, invalid, pristine, reset,
                 </form>
             </CardText>
             <CardText expandable={!submitSucceeded}>
-                <GridList style={gridStyle}>
-                    <Token key={(token===undefined) ? undefined : token.get('id')} token={token}/>
-                </GridList>
-                 <FlatButton label="Done"
+                <Row>
+                    <Col xs={8}>
+                        <Token key={(token===undefined) ? undefined : token.get('id')} token={token}/>
+                    </Col>
+                </Row>
+                <FlatButton label="Done"
                             onClick={cancel}
                             icon={<FontIcon className="fa fa-home" />}/>
             </CardText>
@@ -81,7 +73,7 @@ const Render = ({token, submitSucceeded, handleSubmit, invalid, pristine, reset,
 
 const AddTokenForm = reduxForm({
     form: 'addToken',
-    fields: ['address', 'name', 'symbol', 'decimal']
+    fields: ['address', 'name']
 })(Render);
 
 const AddToken = connect(
@@ -94,7 +86,7 @@ const AddToken = connect(
         return {
             onSubmit: data => {                
                 return new Promise((resolve, reject) => {
-                    dispatch(addToken(data.address, data.name, data.symbol, data.decimal))
+                    dispatch(addToken(data.address, data.name))
                         .then((response) => {
                             resolve(response);
                         });
