5e9f48d3976b119f11b5345d917aad59e4843f9f
Elaine Ou <elaine.ou@gmail.com>
2017-05-06 02:27:55 -0700

    components/contracts/show: screen to interact with contracts

--- src/components/contracts/show.js
+++ src/components/contracts/show.js
@@ -1,50 +1,70 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
-import FlatButton from 'material-ui/FlatButton'
-import FontIcon from 'material-ui/FontIcon'
-import { Row, Col } from 'react-flexbox-grid/lib/index'
-import { DescriptionList, DescriptionTitle, DescriptionData} from '../../elements/dl'
-import QRCode from 'qrcode.react'
-import log from 'loglevel'
-import Immutable from 'immutable'
-import { cardSpace } from '../../lib/styles'
-import { gotoScreen } from '../../store/screenActions'
+import Immutable from 'immutable';
+import { connect } from 'react-redux';
+import { Card, CardTitle, CardHeader, CardText } from 'material-ui/Card';
+import { MenuItem, FlatButton, FontIcon } from 'material-ui';
+import { Row, Col } from 'react-flexbox-grid/lib/index';
+import { DescriptionList, DescriptionTitle, DescriptionData} from '../../elements/dl';
+import log from 'loglevel';
+import { cardSpace } from '../../lib/styles';
+import { gotoScreen } from '../../store/screenActions';
 
-const Render = ({contract, callContract}) => {
+import InteractForm from './interactForm'
+
+const Render = ({contract, functions, inputs, callContract}) => {
+    const style = {
+        fontFamily: 'monospace',
+        letterSpacing: '.02em',
+        margin: '5px',
+        padding: '5px',
+        whiteSpace: 'pre-wrap',
+        wordWrap: 'break-word',
+        outline: '1px solid rgb(224, 224, 224)',
+        overflow: 'auto',
+        maxHeight: '250px'
+      };
 
     return (
         <Card style={cardSpace}>
             <CardHeader
                 title={'Contract: ' + contract.get('name')}
                 subtitle={contract.get('address')}
-                actAsExpander={true}
-                showExpandableButton={true}
             />
-            <CardActions>
-                <FlatButton label="Interact"
-                            onClick={callContract}
-                            icon={<FontIcon className="fa fa-arrow-circle-o-right" />}/>
-            </CardActions>
-            <CardText expandable={true}>
-                <Row>
-                    <Col xs={8}>
-                        <DescriptionList>
-                            <DescriptionTitle>Address:</DescriptionTitle>
-                            <DescriptionData>{contract.get('address')}</DescriptionData>
+            <CardText>
+                <DescriptionList>
+                    <DescriptionTitle>Version: </DescriptionTitle>
+                    <DescriptionData>{contract.get('version')}</DescriptionData>
 
-                        </DescriptionList>
-                    </Col>
-                </Row>
+                    <DescriptionTitle>Options: </DescriptionTitle>
+                    <DescriptionData>{contract.get('options')}</DescriptionData>
+
+                    <DescriptionTitle>Creation Transaction: </DescriptionTitle>
+                    <DescriptionData>{contract.get('txhash')}</DescriptionData>
+                </DescriptionList>
+                <Row>
+                    <b>ABI / JSON Interface</b>
+                    <div style={style}>
+                     {JSON.stringify(contract.get('abi'))} 
+                    </div>
+                </Row>           
+            </CardText>
+            <CardTitle actAsExpander={true}>
+                <FlatButton label="Access"
+                            icon={<FontIcon className="fa fa-arrow-circle-o-right" />}
+                />
+            </CardTitle>
+            <CardText expandable={true}>
+                <InteractForm contract={contract} />
             </CardText>
         </Card>
     )
 };
 
+
+
 const ContractShow = connect(
     (state, ownProps) => {
-        return {
-        }
+
     },
     (dispatch, ownProps) => {
         return {
