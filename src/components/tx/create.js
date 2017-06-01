a238d50e0d4e1b5780385e63a446102ada371ffa
Elaine Ou <elaine.ou@gmail.com>
2017-06-01 14:03:20 -0700

    components/tx/create: store entire tx obj

--- src/components/tx/create.js
+++ src/components/tx/create.js
@@ -92,7 +92,7 @@ const CreateTx = connect(
                     hash: txhash,
                     account: data.from,
                 };
-                dispatch(trackTx(txhash));
+                dispatch(trackTx(Object.assign(data, { hash: txhash })));
                 dispatch(gotoScreen('transaction', txdetails));
             };
             const resolver = (resolve, f) => (x) => {
