096289b7ef78c839ae622ba7acb11f711e8136c2
Elaine Ou <elaine.ou@gmail.com>
2017-06-06 14:43:21 -0700

    components/tx/transaction: refresh tx

--- src/components/tx/transaction.js
+++ src/components/tx/transaction.js
@@ -3,52 +3,62 @@ import { connect } from 'react-redux';
 import Immutable from 'immutable';
 import { TableRow, TableRowColumn } from 'material-ui/Table';
 import { gotoScreen } from 'store/screenActions';
+import { refreshTransactions } from 'store/accountActions';
 import FontIcon from 'material-ui/FontIcon';
 import log from 'loglevel';
 import { link, tables } from 'lib/styles';
 import { toDuration } from 'lib/convert';
 
-const Render = ({ tx, openTx, account, openAccount, refreshTx }) => (
-    <TableRow selectable={false}>
-        <TableRowColumn style={tables.shortStyle}>
-                <span onClick={openTx} style={link}>
-                    {tx.get('value') ? tx.get('value').getEther() : '?'} Ether
-                </span>
-        </TableRowColumn>
-        <TableRowColumn style={tables.shortStyle}>
-                <span onClick={openTx} style={link}>
-                    {tx.get('timestamp') ? toDuration(tx.get('timestamp')) : 'pending'}
-                </span>
-        </TableRowColumn>
-        <TableRowColumn style={tables.wideStyle}>
-                {account && <span onClick={openAccount(account)} style={link}>
-                    {tx.get('from')}
-                </span>}
-                {!account && <span>
-                    {tx.get('from')}
-                </span>}
-        </TableRowColumn>
-        <TableRowColumn style={tables.wideStyle}>
-                <span>
-                    {tx.get('to')}
-                </span>
-        </TableRowColumn>
-        <TableRowColumn style={tables.shortStyle}>
-                <span onClick={refreshTx} style={link}>
-                    <FontIcon className="fa fa-refresh fa-2x" />
-                </span>
-        </TableRowColumn>        
-    </TableRow>
+const Render = ({ tx, openTx, accounts, openAccount, refreshTx }) => {
+
+    const accountRow = (addr) => {
+        const pos = accounts.findKey((acc) => acc.get('id') === addr);
+        const account = (pos >= 0) ? accounts.get(pos) : null;
+        return (
+            <div>
+                {account && <span onClick={() => openAccount(account)} style={link}>
+                            {addr} </span>}
+                {!account && <span> {addr} </span>}
+            </div>
+        );
+    };
+
+
+    return (
+        <TableRow selectable={false}>
+            <TableRowColumn style={tables.shortStyle}>
+                    <span onClick={openTx} style={link}>
+                        {tx.get('value') ? tx.get('value').getEther() : '?'} Ether
+                    </span>
+            </TableRowColumn>
+            <TableRowColumn style={tables.shortStyle}>
+                    <span onClick={openTx} style={link}>
+                        {tx.get('timestamp') ? toDuration(tx.get('timestamp')) : 'pending'}
+                    </span>
+            </TableRowColumn>
+            <TableRowColumn style={tables.wideStyle}>
+                    {accountRow(tx.get('from'))}
+            </TableRowColumn>
+            <TableRowColumn style={tables.wideStyle}>
+                    <span>
+                        {tx.get('to')}
+                    </span>
+            </TableRowColumn>
+            <TableRowColumn style={tables.shortStyle}>
+                    <span onClick={refreshTx} style={link}>
+                        <FontIcon className="fa fa-refresh fa-2x" />
+                    </span>
+            </TableRowColumn>        
+        </TableRow>
     );
+}
 
 const Transaction = connect(
     (state, ownProps) => {
         const accounts = state.accounts.get('accounts', Immutable.List());
-        const pos = accounts.findKey((acc) => acc.get('id') === ownProps.tx.get('from'));
-        const account = (pos > 0) ? accounts.get(pos) : null;
         return {
             tx: ownProps.tx,
-            account,
+            accounts,
         };
     },
     (dispatch, ownProps) => ({
@@ -60,7 +70,8 @@ const Transaction = connect(
             dispatch(gotoScreen('account', acc));
         },
         refreshTx: () => {
-            console.log("rfresh")
+            const hash = ownProps.tx.get('hash');
+            dispatch(refreshTransactions(hash));
         }
     })
 )(Render);
