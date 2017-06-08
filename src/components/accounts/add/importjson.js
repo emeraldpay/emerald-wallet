b48257ce50459d599168f304261d9b2c7b618633
Elaine Ou <elaine.ou@gmail.com>
2017-06-08 13:59:10 -0700

    components/accounts/add/importjson: display error if invalid file

--- src/components/accounts/add/importjson.js
+++ src/components/accounts/add/importjson.js
@@ -16,46 +16,80 @@ import { required } from 'lib/validators';
 import { translate } from 'react-i18next';
 import AccountShow from '../show';
 
-const Render = translate("accounts")(({ t, account, submitSucceeded, handleSubmit, invalid, pristine, reset, submitting, cancel }) => (
-    <Card style={cardSpace}>
-        <CardHeader
-            title={t("import.title")}
-            actAsExpander={false}
-            showExpandableButton={false}
-        />
+class ImportRender extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitFile = this.submitFile.bind(this);
+        this.state = {
+            fileError: null,
+            accountId: null,
+        };
+    }
 
-        <CardText expandable={submitSucceeded}>
-            <form onSubmit={handleSubmit}>
-                <Field name="name"
-                        component={renderTextField}
-                        type="text"
-                        label={t("import.name")} />
-                 <Field name="description"
-                        component={renderTextField}
-                        type="text"
-                        label={t("import.description")} />
-                <Field name="wallet"
-                        component={renderFileField}
-                        validate={required} />
-                <FlatButton label={t("common:submit")} type="submit"
-                            disabled={pristine || submitting || invalid } />
-            </form>
-        </CardText>
-        <CardText expandable={!submitSucceeded}>
-             <AccountShow key={(account === undefined) ? undefined : account.get('id')} account={account}/>
-             <FlatButton label={t("common:done")}
-                        onClick={cancel}
-                        icon={<FontIcon className="fa fa-home" />}/>
-        </CardText>
-        <CardActions>
-            <FlatButton label={t("common:cancel")}
-                        onClick={cancel}
-                        icon={<FontIcon className="fa fa-ban" />}/>
-        </CardActions>
-    </Card>
-));
+    submitFile() {
+        this.props.handleSubmit().then((result) => {
+            if (result.error) {
+                this.setState({ fileError: result.error.toString() });
+            } else {
+                this.setState({ accountId: result });
+            }
+        });
+    }
 
-Render.propTypes = {
+    render() {
+        const { t, accounts } = this.props;
+        const { submitSucceeded, invalid, pristine, submitting, cancel } = this.props;
+
+        const p = this.state.accountId && accounts.findKey(this.state.accountId);
+        const account = p && (p >= 0) && accounts.get(p);
+
+        return (
+            <Card style={cardSpace}>
+                <CardHeader
+                    title={t("import.title")}
+                    actAsExpander={false}
+                    showExpandableButton={false}
+                />
+
+                <CardText expandable={submitSucceeded}>
+                    <form>
+                        <Field name="name"
+                                component={renderTextField}
+                                type="text"
+                                label={t("import.name")} />
+                         <Field name="description"
+                                component={renderTextField}
+                                type="text"
+                                label={t("import.description")} />
+                        <Field name="wallet"
+                                component={renderFileField}
+                                validate={required} />
+                        <FlatButton label={t("common:submit")}
+                                    onClick={this.submitFile}
+                                    disabled={pristine || submitting || invalid } />
+                    </form>
+                </CardText>
+                {account && <CardText>
+                     <AccountShow key={(account === undefined) ? undefined : account.get('id')} account={account}/>
+                     <FlatButton label={t("common:done")}
+                                onClick={cancel}
+                                icon={<FontIcon className="fa fa-home" />}/>
+                </CardText>}
+                {this.state.fileError}
+                {this.state.fileError && <CardText>
+                    {this.state.fileError}
+                </CardText>}
+                <CardActions>
+                    <FlatButton label={t("common:cancel")}
+                                onClick={cancel}
+                                icon={<FontIcon className="fa fa-ban" />}/>
+                </CardActions>
+            </Card>
+        );
+    }
+}
+
+ImportRender.propTypes = {
     account: PropTypes.object.isRequired,
     submitSucceeded: PropTypes.bool.isRequired,
     handleSubmit: PropTypes.func.isRequired,
@@ -66,18 +100,25 @@ Render.propTypes = {
     cancel: PropTypes.func.isRequired,
 };
 
+const ImportRenderT = translate("accounts")(ImportRender);
+
 const ImportAccountForm = reduxForm({
     form: 'importjson',
     fields: ['wallet'],
-})(Render);
+})(ImportRenderT);
 
 const ImportAccount = connect(
     (state, ownProps) => ({
         account: state.accounts.get('accounts', Immutable.List()).last(),
+        accounts: state.accounts.get('accounts', Immutable.List()),
     }),
     (dispatch, ownProps) => ({
         onSubmit: (data) => {
-            dispatch(importWallet(data.wallet[0], data.name, data.description))
+            return new Promise((resolve, reject) => {
+                dispatch(importWallet(data.wallet[0], data.name, data.description))
+                        .then((response) => resolve(response))
+                        .catch((response) => resolve(response));
+            });
         },
         cancel: () => {
             dispatch(gotoScreen('home'));
