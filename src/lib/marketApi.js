84371d1cd6382f19d4f35677615b960531ed57a5
Elaine Ou <elaine.ou@gmail.com>
2017-06-13 14:32:53 -0700

    lib/marketApi: add api for exchange rates

---
+++ src/lib/marketApi.js
@@ -0,0 +1,43 @@
+import 'isomorphic-fetch';
+import log from 'loglevel';
+
+const url = 'https://coinmarketcap-nexuist.rhcloud.com/api/';
+const currency = 'etc';
+
+export class MarketApi {
+
+    call() {
+        return new Promise((resolve, reject) => {
+            this.getRates(currency).then((json) => {
+                if (json.price) {
+                    resolve(json.price);
+                } else {
+                    reject(new Error(`Unknown JSON RPC response: ${json}`));
+                }
+            }).catch((error) => reject(error));
+        });
+    }
+
+    getRates(curr) {
+        return fetch(url + curr)
+            .then((response) => response.json());
+    }
+}
+
+/**
+ * Creates rpc client instance or get
+ * instance from Electron's main process
+ */
+function create() {
+    if (typeof window.process !== 'undefined') {
+        log.debug(`Electron: ${window.process.versions.electron}`);
+        log.debug('Trying to get rpc api from Electron');
+        // request rpc instance from Electron
+        const remote = global.require('electron').remote;
+        return remote.getGlobal('rpc');
+    }
+    return new MarketApi();
+}
+const getRates = create();
+
+export default getRates;
