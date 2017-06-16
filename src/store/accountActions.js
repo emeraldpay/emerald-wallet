e6bf1c76bc78f5d8b24a8479a95bd168dac42f18
Elaine Ou <elaine.ou@gmail.com>
2017-06-16 00:03:01 -0700

    store/accountACtions,accountReducers: edit account fxns

--- src/store/accountActions.js
+++ src/store/accountActions.js
@@ -74,6 +74,22 @@ export function createAccount(password, name, description) {
         });
 }
 
+export function updateAccount(address, name, description) {
+    return (dispatch) =>
+        rpc.call('emerald_updateAccounts', [{
+            name,
+            description,
+            address
+        }]).then((result) => {
+            dispatch({
+                type: 'ACCOUNT/UPDATE_ACCOUNT',
+                address,
+                name,
+                description,
+            });
+        });
+}
+
 export function sendTransaction(accountId, password, to, gas, gasPrice, value) {
     return (dispatch) =>
         rpc.call('eth_sendTransaction', [{
