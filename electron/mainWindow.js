ee014875b399c46a7e78f1bc15d3c5c6f2e371a9
ia <isaac.ardis@gmail.com>
2017-06-21 14:02:30 -0500

    electron: more lint fmt

--- electron/mainWindow.js
+++ electron/mainWindow.js
@@ -8,33 +8,33 @@ import winLinuxMenu from './menus/win-linux';
 export let mainWindow;
 let menu;
 
-export function createWindow (openDevTools) {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1060, height: 600});
+export function createWindow(openDevTools) {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1060, height: 600});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, '../build/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '../build/index.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
 
-  // Open the DevTools.
-  if (openDevTools) {
-    mainWindow.webContents.openDevTools()
-  }
+    // Open the DevTools.
+    if (openDevTools) {
+        mainWindow.webContents.openDevTools();
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 
-  // Menu (only win and linux for now)
-  menu = Menu.buildFromTemplate(winLinuxMenu(mainWindow));
-  mainWindow.setMenu(menu);
+    // Menu (only win and linux for now)
+    menu = Menu.buildFromTemplate(winLinuxMenu(mainWindow));
+    mainWindow.setMenu(menu);
 
-  return mainWindow.webContents;
+    return mainWindow.webContents;
 }
