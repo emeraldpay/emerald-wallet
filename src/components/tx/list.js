f0e208409df3dc8839bfcc46d5a5fcae235129cc
ia <isaac.ardis@gmail.com>
2017-06-22 19:41:44 -0500

    problem: tx list should not simply be empty
    solution: send tx button when there is an account,
    otherwise button to create account

--- src/components/tx/list.js
+++ src/components/tx/list.js
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow } from 'material-ui/Table';
-import { Card, CardHeader, CardText } from 'material-ui/Card';
+import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
+import { Row, Col } from 'react-flexbox-grid/lib/index';
 import FontIcon from 'material-ui/FontIcon';
+import FlatButton from 'material-ui/FlatButton';
 import Avatar from 'material-ui/Avatar';
 import log from 'loglevel';
 import { cardSpace, tables, noShadow, grayBackground } from 'lib/styles';
 import Immutable from 'immutable';
 import Transaction from './transaction';
+import { gotoScreen } from 'store/screenActions';
+import { align } from 'lib/styles';
 
-const Render = ({ transactions }) => {
+const Render = ({ transactions, sendTx, accounts, createAccount }) => {
     const styles = {
         root: {
             display: 'flex',
@@ -49,9 +53,28 @@ const Render = ({ transactions }) => {
                     actAsExpander={false}
                     showExpandableButton={false}
                 />
-                <CardText style={styles.root} expandable={false}>
-                    {table}
-                </CardText>
+                <CardActions style={align.center}>
+                {
+                    (() => {
+                        if (transactions.length) {
+                            return <CardText style={styles.root} expandable={false}>
+                                {table}
+                            </CardText>
+                        }
+                        if (accounts.length && accounts[0] !== Immutable.Map({})) {
+                            return <FlatButton label="Send A Transaction"
+                                icon={<FontIcon className="fa fa-play-circle" />}
+                                style={{backgroundColor: 'limegreen', color: 'white'}}
+                                onClick={sendTx(accounts[0])}/>
+                        }
+                        return <FlatButton label="Create an Account"
+                                icon={<FontIcon className="fa fa-play-circle" />}
+                                style={{backgroundColor: 'limegreen', color: 'white'}}
+                                onClick={createAccount}/>
+                    })()
+                    }
+                }
+                </CardActions>
             </Card>
         </div>
     );
@@ -59,17 +82,28 @@ const Render = ({ transactions }) => {
 
 Render.propTypes = {
     transactions: PropTypes.object.isRequired,
+    accounts: PropTypes.object.isRequired,
+    createAccount: PropTypes.func.isRequired,
+    sendTx: PropTypes.func.isRequired,
 };
 
 const TransactionsList = connect(
     (state, ownProps) => {
         const transactionsAccounts = state.accounts.get('trackedTransactions', Immutable.List());
         const txs = ownProps.transactions || transactionsAccounts;
+        const accounts = state.accounts.get('accounts');
         return {
             transactions: txs.reverse(),
+            accounts,
         };
     },
     (dispatch, ownProps) => ({
+        sendTx: (acc) => {
+            dispatch(gotoScreen('create-tx', acc));
+        },
+        createAccount: () => {
+            dispatch(gotoScreen('generate'));
+        },
     })
 )(Render);
 
