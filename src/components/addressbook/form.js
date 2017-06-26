155953de7e59d9de54793cd2d831bcb1b69aee2e
Wei Tang <hi@that.world>
2017-06-26 15:05:05 +0800

    i18n: initialize support for addressbook components

--- src/components/addressbook/form.js
+++ src/components/addressbook/form.js
@@ -4,35 +4,36 @@ import { renderTextField } from 'elements/formFields';
 import { Card, CardText, FlatButton } from 'material-ui';
 import { required, address } from 'lib/validators';
 import { cardSpace } from 'lib/styles';
+import { translate } from 'react-i18next';
 
-const Render = ({ handleSubmit, blockAddress, invalid, pristine, submitting, cancel }) => (
+const Render = translate('addressbook')(({ t, handleSubmit, blockAddress, invalid, pristine, submitting, cancel }) => (
       <Card style={cardSpace}>
         <CardText expandable={false}>
             <form onSubmit={handleSubmit}>
                 <Field name="address"
                         component={renderTextField}
                         type="text"
-                        label="Network Address"
+                        label={t('form.networkAddress')}
                         disabled={blockAddress}
                         validate={[required, address]} />
                 <Field name="name"
                         component={renderTextField}
                         type="text"
-                        label="Address Name"
+                        label={t('form.addressName')}
                         validate={ required } />
                 <Field name="description"
                         component={renderTextField}
                         type="text"
-                        label="Address Description" />
-                <FlatButton label="Save" type="submit"
+                        label={t('form.addressDescription')} />
+                <FlatButton label={t('common:save')} type="submit"
                             disabled={pristine || submitting || invalid } />
-                <FlatButton label="Cancel"
+                <FlatButton label={t('common:cancel')}
                             secondary={true}
                             onClick={cancel} />
             </form>
         </CardText>
       </Card>
-    );
+    ));
 
 export const AddressForm = reduxForm({
     form: 'AddressForm',
