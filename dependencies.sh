ec4ab6905b5e3c29a91e59e80fa9f62205be6987
ia <isaac.ardis@gmail.com>
2017-06-27 13:23:35 -0500

    revise dependencies script for non-interactive rustup

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get cargo.
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -y
 
 # Ensure cargo bin is added to PATH.
 export PATH=$PATH:~/.cargo/bin
@@ -11,8 +11,10 @@ cargo install emerald-cli
 
 # Get location of emerald.
 # Should be ~/.cargo/bin/emerald, hopefully.
-local emerald_path=$(which emerald)
+emerald_path=$(which emerald)
 
 # We'll just put it in the project base dir for now.
 # Note that this assumes CI's $CWD is in the project root.
 mv "$emerald_path" ./emerald
+
+unset emerald_path
