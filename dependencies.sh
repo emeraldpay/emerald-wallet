3787313b6b22688c23c5f933d1fd0bfc8517aca5
ia <isaac.ardis@gmail.com>
2017-06-27 18:46:22 -0500

    problem: rustup script noninteractive erroring, and should fix build object
    solution: targets array for linux and noninteractive flags for rustup

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,7 @@
 
 # Get cargo.
 echo "Rusting up (non-interactively; accepting defautls)..."
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Ensure cargo bin is added to PATH.
 export PATH=$PATH:~/.cargo/bin
@@ -10,15 +10,16 @@ echo "PATH -> $PATH"
 
 # Install emerald.
 echo "cargo install emerald-cli"
-cargo install emerald-cli
+cargo install emerald-cli -f
 
 # Get location of emerald.
 # Should be ~/.cargo/bin/emerald, hopefully.
 emerald_path=$(which emerald)
-echo "emerald installed to $emerald_path"
+echo "Emerald installed to $emerald_path"
 
 # We'll just put it in the project base dir for now.
 # Note that this assumes CI's $CWD is in the project root.
+echo "Moving emerald from $emerald_path to project directory..."
 mv "$emerald_path" ./emerald
 
 unset emerald_path
