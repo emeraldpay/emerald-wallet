dacc501f5e2c08e65b48ff38a9179a726fd555c6
Igor Artamonov <igor@artamonov.ru>
2017-06-27 15:22:38 -0400

    problem: `(name || "_unknown_").toLowerCase is not a function`

--- src/lib/networks.js
+++ src/lib/networks.js
@@ -7,7 +7,7 @@ export const Networks = [
 ];
 
 export function findNetworkDetails(name, chainId, type) {
-    let nameClean = (name || "_unknown_").toLowerCase();
+    let nameClean = (name || "_unknown_").toString().toLowerCase();
     let found = Networks.find((n) => (n.name === nameClean || n.chainId === chainId) && n.type === type);
     if (!found) {
         log.error("Unknown network", name, chainId, type);
