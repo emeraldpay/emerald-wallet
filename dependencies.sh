59da64409fd56805ff02de0579b7d59b865f91c5
ia <isaac.ardis@gmail.com>
2017-06-28 08:27:51 -0500

    problem: dependencies script for CI should be more verbose
    solution: fix typos and format echos

--- dependencies.sh
+++ dependencies.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 # Get cargo.
-echo "Rusting up (non-interactively; accepting defautls)..."
+echo "Rusting up (non-interactively; accepting defaults)..."
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # Ensure cargo bin is added to PATH.
 export PATH=$PATH:~/.cargo/bin
+echo "Added cargo to path..."
 echo "PATH -> $PATH"
 
 # Install emerald.
-echo "cargo install emerald-cli"
+echo "Installing emerald with cargo..."
+echo "$ cargo install emerald-cli"
 cargo install emerald-cli -f
 
 # Get location of emerald.
 # Should be ~/.cargo/bin/emerald, hopefully.
 emerald_path=$(which emerald)
-echo "Emerald installed to $emerald_path"
+echo "Emerald installed to [$emerald_path]"
 
 # We'll just put it in the project base dir for now.
 # Note that this assumes CI's $CWD is in the project root.
-echo "Moving emerald from $emerald_path to project directory..."
+echo "Moving emerald from [$emerald_path] to project directory [$(pwd)]..."
 mv "$emerald_path" ./emerald
 
 unset emerald_path
