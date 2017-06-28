0c281fa6647f7be0b4c80f5e14abf69a97acacad
ia <isaac.ardis@gmail.com>
2017-06-28 12:19:56 -0500

    problem: should capture logs from geth and emerald
    solution: dont depend on process.stderr.data to ensure running
     - geth: use '--log-dir' flag to capture logs
     - emerald: pipe stderr to fs.createWriteStream
    
    use getLogDir() function to place geth and emerald logs in electron userData/logs path
    for production, in ./logs for development
    
    - also fixes formatting errors
    
    Fixes #178
    Fixes #188

--- electron/launcher.js
+++ electron/launcher.js
@@ -3,22 +3,23 @@ import fs from 'fs';
 import os from 'os';
 import log from 'electron-log';
 import path from 'path';
+import { getLogDir } from './services';
 
 const suffix = os.platform() === 'win32' ? '.exe' : '';
 
-function checkExists(target) {
+export function checkExists(target) {
     return new Promise((resolve) => {
         fs.access(target, fs.constants.R_OK | fs.constants.X_OK, (err) => {
             if (err) {
-                resolve(false)
+                resolve(false);
             } else {
-                fs.stat(target, (err, stat) => {
-                    if (err) {
-                        resolve(false)
+                fs.stat(target, (e, stat) => {
+                    if (e) {
+                        resolve(false);
                     } else if (!stat.isFile() || stat.size === 0) {
-                        resolve(false)
+                        resolve(false);
                     } else {
-                        resolve(true)
+                        resolve(true);
                     }
                 });
             }
@@ -36,25 +37,27 @@ export class LocalGeth {
     launch() {
         return new Promise((resolve, reject) => {
             log.info(`Starting Geth... [network: ${this.network}, port: ${this.rpcPort}]`);
-            const bin = path.join(this.bin, 'geth' + suffix);
+            const bin = path.join(this.bin, `geth${suffix}`);
             fs.access(bin, fs.constants.X_OK, (err) => {
                 if (err) {
                     log.error(`File ${bin} doesn't exist or app doesn't have execution flag`);
-                    reject(err)
+                    reject(err);
                 } else {
-                    let options = [
+                    const logTarget = path.join(getLogDir(), 'geth'); // this shall be a dir
+                    const options = [
                         '--chain', this.network,
                         '--rpc',
                         '--rpc-port', this.rpcPort,
                         '--rpc-cors-domain', 'http://localhost:8000',
                         '--cache=128',
                         '--fast', // (auto-disables when at or upon reaching current bc height)
+                        '--log-dir', logTarget,
                     ];
                     this.proc = spawn(bin, options);
-                    resolve(this.proc)
+                    resolve(this.proc);
                 }
             });
-        })
+        });
     }
 
     shutdown() {
@@ -122,13 +125,12 @@ export class LocalConnector {
     // which is the project base dir.
     migrateIfNotExists() {
         return new Promise((resolve, reject) => {
-            const bin = path.join(this.bin, 'emerald' + suffix);
+            const bin = path.join(this.bin, `emerald${suffix}`);
             log.debug('Checking if emerald exists:', bin);
             checkExists(bin).then((exists) => {
                 if (!exists) {
                     log.debug('emerald not found');
                     // check that included binary path exists
-                    // where packaged emerald binary path => app.getAppPath()/Contents/Emerald
                     // if it does exist, move it to this.bin/
                     const cargoEmeraldPath = path.join(process.env.HOME, '.cargo', 'bin', 'emerald');
                     log.debug('cargo installed emerald path:', cargoEmeraldPath);
@@ -156,23 +158,20 @@ export class LocalConnector {
                 // Assuming the emerald found is valid (perms, etc).
                 log.debug('OK: emerald exists: ', bin);
                 resolve(true);
-            })
+            });
         });
     }
 
     start() {
         return new Promise((resolve, reject) => {
-            const bin = path.join(this.bin, 'emerald' + suffix);
+            const bin = path.join(this.bin, `emerald${suffix}`);
             fs.access(bin, fs.constants.F_OK | fs.constants.R_OK | fs.constants.X_OK, (err) => {
                 if (err) {
                     log.error(`File ${bin} doesn't exist or app doesn't have execution flag`);
-                    reject(err)
+                    reject(err);
                 } else {
-                    let options = [
+                    const options = [
                         'server',
-                        // '--verbose', 1,
-                        // '--chain-id', this.chainId
-                        // '--chain', 'testnet',
                     ];
                     this.proc = spawn(bin, options);
                     resolve(this.proc);
@@ -184,7 +183,6 @@ export class LocalConnector {
     launch() {
         return new Promise((resolve, reject) => {
             log.info(`Starting Emerald Connector... [chainId: ${this.chainId}]`);
-            const bin = path.join(this.bin, 'emerald' + suffix);
             this.migrateIfNotExists()
                     .then(this.start.bind(this))
                     .then(resolve)
