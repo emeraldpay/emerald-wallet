c0dabbf10611384d3ec306aef9c5be7b86896e10
ia <isaac.ardis@gmail.com>
2017-06-29 13:38:20 -0500

    problem: should increase logging levels for Alpha dist
    solution: increment them, especially the one to file

--- electron/main.js
+++ electron/main.js
@@ -15,7 +15,7 @@ const isProd = process.env.NODE_ENV === 'production';
 // on Linux: ~/.config/<app name>/log.log
 // on OS X: ~/Library/Logs/<app name>/log.log
 // on Windows: %USERPROFILE%\AppData\Roaming\<app name>\log.log
-log.transports.file.level = isDev ? 'debug' : 'info';
+log.transports.file.level = isDev ? 'silly' : 'debug';
 log.transports.console.level = isDev ? 'debug' : 'info';
 
 const settings = new Store({
