13136efcba8f03c14127534133ac9e695bc68f04
Elaine Ou <elaine.ou@gmail.com>
2017-07-13 02:20:35 -0700

    components/tx/create: rearrange sendtx form

--- src/components/tx/createTxForm.js
+++ src/components/tx/createTxForm.js
@@ -5,19 +5,79 @@ import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import { SelectField, TextField, RadioButtonGroup } from 'redux-form-material-ui';
 import { RadioButton } from 'material-ui/RadioButton';
 import { MenuItem, FlatButton, FontIcon, IconButton } from 'material-ui';
-import Divider from 'material-ui/Divider';
-import { IconMenu } from 'material-ui/IconMenu';
-import ImportContacts from 'material-ui/svg-icons/communication/import-contacts';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
+import { IconMenu } from 'material-ui/IconMenu';
+import KeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
+import ImportContacts from 'material-ui/svg-icons/communication/import-contacts';
 import { red200 } from 'material-ui/styles/colors';
-import { cardSpace } from 'lib/styles';
 import { positive, number, required, address } from 'lib/validators';
 
-/**
-  TODO (elaine):
-    `balance` is a temporary placeholder.
-    Replace with card containing full account data.
-****/
+const CardHeadEmerald = (props) => {
+  const { title, backLabel, cancel } = props;
+  const style = {
+    color: '#191919',
+    fontFamily: 'GT Walsheim Pro',
+    fontSize: '22px',
+    lineHeight: '24px',
+    textAlign: 'left',
+  }
+
+  const flatButtonNav = {
+    color: '#747474',
+    fontFamily: 'GT Walsheim Pro',
+    fontSize: '14px',
+    fontWeight: '500',
+    lineHeight: '24px',
+  };
+
+  return (
+      <Row middle="xs" style={{marginBottom: '60px'}}>
+        <Col xs={4}>
+          <FlatButton label={backLabel}
+                      primary={true}
+                      onClick={cancel}
+                      style={flatButtonNav}
+                      icon={<KeyboardArrowLeft/>}
+          />
+        </Col>
+        <Col xs={6} style={style}>{title}</Col>
+      </Row>
+  );
+
+}
+
+const formLabel = {
+    color: '#747474', 
+    fontFamily: "GT Walsheim Pro", 
+    fontSize: '18px',
+    lineHeight: '24px',
+    textAlign: 'right',
+    marginRight: '30px',
+}
+
+const formGroup = {
+  marginTop: '10px',
+}
+
+const formInput = {
+    boxSizing: 'border-box',
+    height: '51px', 
+    border: '1px solid #DDDDDD',
+    borderRadius: '1px',
+    color: '#191919',
+    fontFamily: "GT Walsheim Pro", 
+    fontSize: '16px',
+    lineHeight: '24px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+}
+
+const submitButton = {
+    fontFamily: "GT Walsheim Pro",
+    fontSize: '14px',
+    fontWeight: '500', 
+    borderRadius: '1px',
+}
 
 const Render = (props) => {
     const { fields: { from, to }, accounts, balance, handleSubmit, invalid, pristine, submitting } = props;
@@ -25,61 +85,61 @@ const Render = (props) => {
     const { error, cancel } = props;
 
     return (
-    <Card style={cardSpace}>
-      <CardActions>
-          <FlatButton label="ACCOUNT"
-                      primary={true}
-                      onClick={cancel}
-                      icon={<FontIcon className="fa fa-arrow-left" />}/>
-      </CardActions>
-      <CardHeader
-        title='Send Transaction'
-        actAsExpander={false}
-        showExpandableButton={false}
+    <Card>
+      <CardHeadEmerald 
+        backLabel='DASHBOARD'
+        title='Send Ether & Tokens'
+        cancel={cancel}
       />
-
       <CardText expandable={false}>
         <Row>
           <Col xs={12} md={8}>
-            <Row>
-              <Col xs={12}>
+            <Row middle="xs">
+              <Col xs={2} xsOffset={1} style={formLabel}>
+                From
+              </Col>
+              <Col xs={8} style={formGroup}>
                 <Field name="from"
-                       floatingLabelText="From"
+                       style={formInput}
                        onChange={(event, val) => onChangeAccount(accounts, val)}
                        component={SelectField}
+                       underlineShow={false}
                        fullWidth={true}>
                   {accounts.map((account) =>
                     <MenuItem key={account.get('id')} value={account.get('id')} primaryText={account.get('id')} />
                   )}
                 </Field>
-                <Field name="balance"
-                       disabled={true}
-                       component={TextField}
-                       floatingLabelText="Balance"
-                />
               </Col>
             </Row>
-            <Row>
-              <Col xs={12}>
+            <Row middle="xs">
+              <Col xs={2} xsOffset={1} style={formLabel}>
+                Password
+              </Col>            
+              <Col xs={8} style={formGroup}>
                 <Field name="password"
-                       floatingLabelText="Password"
+                       style={formInput}
                        type="password"
                        component={TextField}
+                       underlineShow={false}
                        fullWidth={true}
                        validate={required}>
                 </Field>
               </Col>
             </Row>
-            <Row>
-              <Col xs={11}>
+            <Row middle="xs">
+              <Col xs={2} xsOffset={1} style={formLabel}>
+                To
+              </Col>
+              <Col xs={7} style={formGroup}>
                 <Field name="to"
+                       style={formInput}
                        component={TextField}
-                       floatingLabelText="Target Address"
                        validate={[required, address]}
+                       underlineShow={false}
                        fullWidth={true}
                 />
               </Col>
-              <Col xs={1}>
+              <Col xs={1} style={formGroup}>
                 <IconMenu
                     iconButtonElement={<IconButton><ImportContacts /></IconButton>}
                     onItemTouchTap={handleSelect}
@@ -98,24 +158,30 @@ const Render = (props) => {
                 </IconMenu>
               </Col>
             </Row>
-            <Row>
-              <Col xs={8}>
+            <Row middle="xs">
+              <Col xs={2} xsOffset={1} style={formLabel}>
+                Amount
+              </Col>
+              <Col xs={5} style={formGroup}>
                 <Field name="value"
+                       style={formInput}
                        component={TextField}
-                       floatingLabelText="Amount"
                        hintText="1.0000"
                        fullWidth={true}
+                       underlineShow={false}
                        validate={[required]}
                 />
               </Col>
-              <Col xs={4} style={{paddingTop: "24px"}}>
+              <Col xs={2} style={formGroup}>
                 <Field name="token"
                        component={SelectField}
                        onChange={onChangeToken}
                        value={token}
+                       underlineShow={false}
                        fullWidth={true}>
                   {tokens.map((it) =>
                     <MenuItem key={it.get('address')}
+                              style={formInput}
                               value={it.get('address')}
                               label={it.get('symbol')}
                               primaryText={it.get('symbol')} />
@@ -123,8 +189,9 @@ const Render = (props) => {
                 </Field>
               </Col>
               {isToken &&
-              <Col>
+              <Col xs={2} style={formGroup}>
                 <Field name="isTransfer"
+                       style={formInput}
                        component={RadioButtonGroup}
                        defaultSelected="true"
                        validate={required}>
@@ -133,20 +200,21 @@ const Render = (props) => {
                 </Field>
               </Col> }
             </Row>
-          </Col>
-
-          <Col xs={12} md={4}>
-            <Row>
-              <Col xs={12}>
+            <Row middle="xs">
+              <Col xs={2} xsOffset={1} style={formLabel}>
+                Fee
+              </Col>
+              <Col xs={5} style={formGroup}>
                 <Field name="gasPrice"
                        component={TextField}
-                       floatingLabelText="Gas Price (MGas)"
-                       hintText="10000"
+                       hintText="23000"
+                       style={formInput}
+                       underlineShow={false}
                        validate={[required, number, positive]}
                 />
               </Col>
             </Row>
-            <Row>
+            {/*<Row>
               <Col xs={12}>
                 <Field name="gas"
                        component={TextField}
@@ -155,6 +223,20 @@ const Render = (props) => {
                        validate={[required, number, positive]}
                 />
               </Col>
+            </Row>*/}
+          </Col>
+
+          <Col xs={12} md={4}>
+            <Row>
+              <Col xs={11}>
+                <Field name="balance"
+                       disabled={true}
+                       component={TextField}
+                       floatingLabelText="Balance"
+                />
+
+              </Col>
+
             </Row>
           </Col>
         </Row>
@@ -167,17 +249,20 @@ const Render = (props) => {
         )}
       </CardText>
 
-      <CardActions>
-        <FlatButton label="Cancel"
-                    onClick={cancel}
-                    icon={<FontIcon className="fa fa-ban" />}
-                    secondary={true} />
-        <FlatButton label="Send"
-                    disabled={ pristine || submitting || invalid }
-                    onClick={handleSubmit}
-                    icon={<FontIcon className="fa fa-check" />}
-                     />
-      </CardActions>
+
+      <Row>
+        <Col xs={8} xsOffset={3}>
+          <FlatButton label="Send"
+                      disabled={ pristine || submitting || invalid }
+                      onClick={handleSubmit}
+                      style={submitButton}
+                      backgroundColor="#47B04B"
+                       />
+          <FlatButton label="Cancel"
+                      onClick={cancel}
+                      backgroundColor="#DDD" />                     
+        </Col>              
+      </Row>
     </Card>
     );
 };
