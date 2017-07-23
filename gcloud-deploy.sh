8718ca9eb211f80e99b4b108b26065f2a9a513dd
Igor Artamonov <igor@artamonov.ru>
2017-07-23 10:10:49 -0400

    solution: deploy all in one call

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -3,9 +3,9 @@
 VERSION_BASE=$(janus version -format='v%M.%m.x')
 echo "Deploy to http://builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/"
 
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="./dist/*.zip" -key="./gcloud-travis.json.enc"
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="./dist/*.dmg" -key="./gcloud-travis.json.enc"
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="./dist/*.tar.gz" -key="./gcloud-travis.json.enc"
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="./dist/*.deb" -key="./gcloud-travis.json.enc"
+mkdir deploy
+mv ./dist/*.dmg ./dist/*.tar.gz ./dist/*.deb ./dist/*.zip ./deploy/
+
+janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="./dist/*" -key="./gcloud-travis.json.enc"
 
 echo "Deployed"
