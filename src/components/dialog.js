d47442e9d497843e98022db4d4a212e0b54663ac
Igor Artamonov <igor@artamonov.ru>
2017-07-29 23:10:09 -0400

    problem: not redux

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -3,13 +3,16 @@ import { connect } from 'react-redux';
 import log from 'electron-log';
 
 import WaitForSign from './tx/waitForSignDialog';
+import Receive from './accounts/receiveDialog';
 
-const Render = ({ dialog }) => {
+const Render = ({ dialog, item }) => {
 
     if (!dialog) {
         return <div/>
     } else if (dialog === 'sign-transaction') {
         return <WaitForSign/>
+    } else if (dialog === 'receive') {
+        return <Receive account={item}/>
     } else {
         log.error("Unsupported dialog", dialog);
         return <div/>
@@ -19,6 +22,7 @@ const Render = ({ dialog }) => {
 const Error = connect(
     (state, ownProps) => ({
         dialog: state.screen.get('dialog'),
+        item: state.screen.get('dialogItem')
     }),
     (dispatch, ownProps) => ({
     })
