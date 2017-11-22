2a1d523c89130c064bb059897417b8397478f7a7
gagarin55 <gagarin55@protonmail.com>
2017-11-22 19:54:36 +0300

    Import mnemonic dialog refinement

--- src/components/accounts/add/ImportMnemonic/importMnemonic.js
+++ src/components/accounts/add/ImportMnemonic/importMnemonic.js
@@ -7,7 +7,6 @@ import TextField from 'elements/Form/TextField';
 import DashboardButton from 'components/common/DashboardButton';
 import accounts from 'store/vault/accounts';
 import screen from 'store/wallet/screen';
-
 import Button from 'elements/Button';
 import { Warning, WarningHeader, WarningText } from 'elements/Warning';
 
@@ -44,72 +43,91 @@ class ImportMnemonic extends React.Component {
   render() {
     const { onBack } = this.props;
     return (
-            <Form caption="Import Mnemonic" backButton={ <DashboardButton onClick={ onBack }/> }>
-                <form>
-                    <Row>
-                        <div style={formStyles.left}/>
-                        <div style={formStyles.right}>
-                            <div style={{width: '100%'}}>
-                                <div className={styles.passwordLabel}>Enter a strong password</div>
-                                <div className={styles.passwordSubLabel}>This password will be required to confirm all account
-                                    operations.
-                                </div>
-                                <div style={{marginTop: '30px'}}>
-                                    <Field
-                                        name="password"
-                                        type="password"
-                                        component={TextField}
-                                        fullWidth={true}
-                                        underlineShow={false}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </Row>
-                    <Row>
-                        <div style={formStyles.left}/>
-                        <div style={formStyles.right}>
-                            <Warning fullWidth={ true }>
-                                <WarningHeader>Don&#39;t forget it.</WarningHeader>
-                                <WarningText>If you forget this password, you will lose access to the account and its funds.</WarningText>
-                            </Warning>
-                        </div>
-                    </Row>
+      <Form caption="Import Mnemonic" backButton={ <DashboardButton onClick={ onBack }/> }>
+        <Row>
+            <div style={formStyles.left}/>
+            <div style={formStyles.right}>
+                <div style={{width: '100%'}}>
+                    <div className={styles.passwordLabel}>Enter a strong password</div>
+                    <div className={styles.passwordSubLabel}>This password will be required to confirm all account
+                        operations.
+                    </div>
+                    <div style={{marginTop: '30px'}}>
+                        <Field
+                            name="password"
+                            type="password"
+                            component={TextField}
+                            fullWidth={true}
+                            underlineShow={false}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Row>
+        <Row>
+            <div style={formStyles.left}/>
+            <div style={formStyles.right}>
+                <Warning fullWidth={ true }>
+                    <WarningHeader>Don&#39;t forget it.</WarningHeader>
+                    <WarningText>If you forget this password, you will lose access to the account and its funds.</WarningText>
+                </Warning>
+            </div>
+        </Row>
 
-                    <Row>
-                        <div style={formStyles.left}>
-                        </div>
-                        <div style={formStyles.right}>
-                            <div style={{width: '100%'}}>
-                                <div className={ styles.mnemonicLabel }>Enter a mnemonic phrase</div>
-                                <div>
-                                    <Field
-                                        name="mnemonic"
-                                        component={ TextField }
-                                        fullWidth={ true }
-                                        underlineShow={ false }
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </Row>
+        <Row>
+            <div style={formStyles.left}>
+            </div>
+            <div style={formStyles.right}>
+                <div style={{width: '100%'}}>
+                    <div className={ styles.mnemonicLabel }>Enter a mnemonic phrase</div>
+                    <div>
+                        <Field
+                          multiLine={ true }
+                          rowsMax={ 4 }
+                          rows={ 2 }
+                          name="mnemonic"
+                          component={ TextField }
+                          fullWidth={ true }
+                          underlineShow={ false }
+                        />
+                    </div>
+                </div>
+            </div>
+        </Row>
 
-                    <Row>
-                        <div style={formStyles.left}/>
-                        <div style={formStyles.right}>
-                            <Button primary label="Import" onClick={ this.submit }/>
-                        </div>
-                    </Row>
-                    { this.state.error }
-                </form>
-            </Form>
+        <Row>
+          <div style={formStyles.left}>
+          </div>
+          <div style={formStyles.right}>
+            <div style={{width: '100%'}}>
+              <div className={ styles.mnemonicLabel }>HD derivation path</div>
+              <div>
+                <Field
+                  name="hdpath"
+                  component={ TextField }
+                  fullWidth={ true }
+                  underlineShow={ false }
+                />
+              </div>
+            </div>
+          </div>
+        </Row>
+
+        <Row>
+            <div style={formStyles.left}/>
+            <div style={formStyles.right}>
+                <Button primary label="Import" onClick={ this.submit }/>
+            </div>
+        </Row>
+        { this.state.error }
+      </Form>
     );
   }
 }
 
 const importForm = reduxForm({
   form: 'importMnemonic',
-  fields: ['password', 'mnemonic'],
+  fields: ['password', 'mnemonic', 'hdpath'],
 })(ImportMnemonic);
 
 export default connect(
--- src/elements/Form/TextField/textField.js
+++ src/elements/Form/TextField/textField.js
@@ -2,40 +2,39 @@ import React from 'react';
 import { TextField as ReduxFormTextField } from 'redux-form-material-ui';
 
 const defaultStyle = {
-    height: '51px',
-    color: '#191919',
-    fontSize: '16px',
-    lineHeight: '24px',
+  color: '#191919',
+  fontSize: '16px',
+  lineHeight: '24px',
 };
 
 const container = {
-    boxSizing: 'border-box',
-    border: '1px solid',
-    borderColor: '#DDDDDD',
-    borderRadius: '1px',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    display: 'flex',
-    alignItems: 'center',
+  boxSizing: 'border-box',
+  border: '1px solid',
+  borderColor: '#DDDDDD',
+  borderRadius: '1px',
+  paddingLeft: '10px',
+  paddingRight: '10px',
+  display: 'flex',
+  alignItems: 'center',
 };
 
 export const TextField = (props) => {
-    const { rightIcon, invalid, style, ...other } = props;
-    if (other.fullWidth) {
-        container.width = '100%';
-    }
+  const { rightIcon, invalid, style, ...other } = props;
+  if (other.fullWidth) {
+    container.width = '100%';
+  }
 
-    let containerStyle = invalid ? {...container, borderColor: '#BC0000' } : container;
-    if (style) {
-        containerStyle = { ...containerStyle, maxHeight: style.maxHeight, minWidth: style.minWidth };
-    }
+  let containerStyle = invalid ? {...container, borderColor: '#BC0000' } : container;
+  if (style) {
+    containerStyle = { ...containerStyle, maxHeight: style.maxHeight, minWidth: style.minWidth };
+  }
 
-    const textFieldStyle = invalid ? {...defaultStyle, color: '#BC0000' } : defaultStyle;
-    return (
+  const textFieldStyle = invalid ? {...defaultStyle, color: '#BC0000' } : defaultStyle;
+  return (
         <div style={ containerStyle }>
             <ReduxFormTextField { ...other } style={ textFieldStyle } />{ rightIcon }
         </div>
-    );
+  );
 };
 
 export default TextField;
