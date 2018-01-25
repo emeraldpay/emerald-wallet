d88538e456388ccacd5350dd58343b1a6d8a40f5
gagarin55 <gagarin55@protonmail.com>
2018-01-25 17:24:27 +0300

    Circle CI: deployment

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,6 +5,9 @@ set -e
 VERSION_BASE=$(janus version -format='v%M.%m.x')
 echo "Deploy to http://builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/"
 
+echo $PWD
+ls -la
+
 mkdir ../deploy
 mv ../dist/*.dmg ../dist/*.tar.gz ../dist/*.deb ../dist/*.zip ../dist/*.AppImage ../deploy/
 
