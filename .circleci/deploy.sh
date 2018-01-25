e4eced94fe152f1cabdf378854729878266745da
gagarin55 <gagarin55@users.noreply.github.com>
2018-01-25 10:28:09 -0800

    Circle CI: deployment (#453)
    * Circle CI: deployment to google cloud

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,9 +5,10 @@ set -e
 VERSION_BASE=$(janus version -format='v%M.%m.x')
 echo "Deploy to http://builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/"
 
-mkdir ../deploy
-mv ../dist/*.dmg ../dist/*.tar.gz ../dist/*.deb ../dist/*.zip ../dist/*.AppImage ../deploy/
+mkdir deploy
 
-janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="../deploy/*" -key="$GCLOUD_KEY"
+mv dist/*.dmg dist/*.zip deploy/
+
+janus deploy -to="builds.etcdevteam.com/emerald-wallet/$VERSION_BASE/" -files="deploy/*" -key=".circleci/gcloud-circleci.json.enc"
 
 echo "Deployed"
