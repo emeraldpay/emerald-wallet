4e67b591a9c16a853e918f6f1eb902c440fe734d
BelfordZ <belfordz66@gmail.com>
2018-01-24 22:39:33 -0800

    write cargo path update to .profile and only install if needed

--- dependencies.sh
+++ dependencies.sh
@@ -4,39 +4,55 @@ EMERALD_CLI_VER=0.19.0
 
 set -e
 
-# Get cargo.
-if command -v cargo 2>/dev/null; then
-    # Have cargo already.
-    echo "Cargo already installed."
-else
-    # No cargo yet.
-    echo "Rusting up (non-interactively; accepting defaults)..."
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
+exit_if_emerald_installed() {
+    echo "Asserting correct version of emerald is installed"
 
-    # Ensure cargo bin is added to PATH, but only if it's not already there.
-    # So developers don't have to worry about a PATH that's miles long
-    # with repetitious cargo pathos.
-    KARGO_PATH=~/.cargo/bin
-    export PATH="${PATH:+"$PATH:"}$KARGO_PATH"
-    echo "Added cargo to path..."
-    echo "PATH -> $PATH"
-fi
+    # emerald command exists in path?
+    if [ -x "$(command -v emerald)" ]; then
 
-# Install and move emerald.
-echo "Installing emerald with cargo..."
-echo "$ cargo install --vers $EMERALD_CLI_VER emerald-cli"
-export RUSTFLAGS="-C target-feature=+crt-static"
-cargo install --vers $EMERALD_CLI_VER emerald-cli -f
+        # emerald is the correct version?
+        if [ "emerald --version" = EMERALD_CLI_VER ]; then
+            echo "emerald $EMERALD_CLI_VER is already installed. Skipping."
+            exit 0
+        fi
+    fi
+}
 
-# Get location of emerald.
-# Should be ~/.cargo/bin/emerald, hopefully.
-emerald_path=$(which emerald)
-echo "Emerald installed to [$emerald_path]"
+ensure_cargo_installed() {
+    if ! [ -x "$(command -v cargo)" ]; then
+        echo "Rusting up (non-interactively; accepting defaults)..."
+        curl https://sh.rustup.rs -sSf | sh -s -- -y
 
-# We'll just put it in the project base dir for now.
-# Note that this assumes CI's $CWD is in the project root.
-echo "Copying emerald from [$emerald_path] to project directory [$(pwd)]..."
-cp "$emerald_path" ./emerald
-chmod +x emerald
+        echo "Adding cargo to path... (and writing to ~/.profile)"
+        echo $PATH:~/.cargo/bin >> ~/.profile
+        source ~/.profile
+        echo "PATH -> $PATH"
+        echo "Cargo installed Successfully."
+    fi
+}
 
-unset emerald_path
+install_emerald() {
+    echo "Installing emerald with cargo..."
+    echo "$ cargo install --vers $EMERALD_CLI_VER emerald-cli"
+    export RUSTFLAGS="-C target-feature=+crt-static"
+    cargo install --vers $EMERALD_CLI_VER emerald-cli -f
+    unset RUSTFLAGS
+
+    # Get location of emerald.
+    # Should be ~/.cargo/bin/emerald, hopefully.
+    emerald_path=$(which emerald)
+    echo "Emerald installed to [$emerald_path]"
+
+    # We'll just put it in the project base dir for now.
+    # Note that this assumes CI's $CWD is in the project root.
+    echo "Copying emerald from [$emerald_path] to project directory [$(pwd)]..."
+    cp "$emerald_path" ./emerald
+    chmod +x emerald
+
+    unset emerald_path
+}
+
+exit_if_emerald_installed
+
+ensure_cargo_installed
+install_emerald
