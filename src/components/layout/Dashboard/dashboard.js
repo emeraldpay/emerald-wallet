40d2aa90723b50af4f5c306d06b4a477e7f28f2c
gagarin55 <gagarin55@protonmail.com>
2018-01-31 12:45:11 +0300

    problem: When Dashboard shows loading status it should display message from launcher

--- src/components/layout/Dashboard/dashboard.js
+++ src/components/layout/Dashboard/dashboard.js
@@ -8,16 +8,24 @@ import TransactionsHistory from '../../../components/tx/TxHistory';
 import AccountsList from '../../../components/accounts/AccountList';
 import Header from './header';
 
+const styles = {
+  statusMessage: {
+    color: '#999',
+    marginTop: '15px',
+  },
+};
+
 const Dashboard = (props) => {
-  const { connecting } = props;
+  const { connecting, statusMessage } = props;
 
   if (connecting) {
     return (
       <div>
         <Grid>
           <Row center="xs">
-            <Col xs={3}>
-              <i className="fa fa-spin fa-spinner"/> Loading...
+            <Col xs={12}>
+              <div><i className="fa fa-spin fa-spinner"/> Loading...</div>
+              <div style={styles.statusMessage}>{statusMessage}</div>
             </Col>
           </Row>
         </Grid>
@@ -37,12 +45,14 @@ const Dashboard = (props) => {
 
 Dashboard.propTypes = {
   connecting: PropTypes.bool.isRequired,
+  statusMessage: PropTypes.string,
 };
 
 export default connect(
   (state, ownProps) => ({
     accounts: state.accounts.get('accounts', Immutable.List()),
     connecting: state.launcher.get('connecting'),
+    statusMessage: state.launcher.get('message').get('text'),
   }),
   (dispatch, ownProps) => ({
   })
