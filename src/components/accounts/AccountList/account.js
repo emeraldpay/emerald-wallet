31427cf17006241fd9ef88ea694f32a8bcee1298
Shane <jonas.shane@gmail.com>
2018-03-27 22:05:07 -0700

    problem: unnecessary plus sign (#540)
    solution: remove plus sign and showing tokens on dashboard

--- src/components/accounts/AccountList/account.js
+++ src/components/accounts/AccountList/account.js
@@ -4,16 +4,11 @@ import { CardText } from 'material-ui/Card';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
 import { Button, IdentityIcon, Account as AddressAvatar, ButtonGroup, Card } from 'emerald-js-ui';
 import { QrCode as QrCodeIcon } from 'emerald-js-ui/lib/icons2';
-import { connect } from 'react-redux';
-
-import tokens from '../../../store/vault/tokens';
 import SecondaryMenu from '../SecondaryMenu';
 import AccountBalance from '../Balance';
-import TokenBalances from '../TokenBalances';
 import TokenUnits from '../../../lib/tokenUnits';
 
 import styles from './account.scss';
-
 
 const qrIconStyle = {
   width: '14px',
@@ -30,19 +25,6 @@ export class Account extends React.Component {
       showFiat: PropTypes.bool,
     };
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        showTokens: false,
-      };
-    }
-
-    switchDetails = () => {
-      this.setState((prevState) => ({
-        showTokens: !prevState.showTokens,
-      }));
-    }
-
     onSendClick = () => this.props.createTx(this.props.account);
 
     onAddressClick = () => this.props.openAccount(this.props.account);
@@ -50,9 +32,8 @@ export class Account extends React.Component {
     onAddEtcClick = () => this.props.showReceiveDialog(this.props.account);
 
     render() {
-      const { account, tokensBalances } = this.props;
+      const { account } = this.props;
       const { showFiat } = this.props;
-      const { showTokens } = this.state;
 
       // TODO: we convert Wei to TokenUnits here
       const balance = account.get('balance') ? new TokenUnits(account.get('balance').value(), 18) : null;
@@ -63,11 +44,7 @@ export class Account extends React.Component {
             <Row>
               <Col xs={3}>
                 <div className={ styles.identityIconContainer }>
-                  <IdentityIcon
-                    id={ account.get('id') }
-                    onClick={ this.switchDetails }
-                    expanded={ showTokens }
-                  />
+                  <IdentityIcon id={ account.get('id') } expanded={true}/>
                   <div style={{marginLeft: '10px'}}>
                     {balance && <AccountBalance
                       balance={ balance }
@@ -105,30 +82,9 @@ export class Account extends React.Component {
                 </div>
               </Col>
             </Row>
-            { showTokens &&
-                    (<div>
-                      <Row>
-                        <Col xs={12} lg={12} md={12} sm={12}>
-                          <hr className={ styles.tokensDivider } />
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col xs={12} lg={12} md={12} sm={12} style={{marginLeft: '50px'}}>
-                          <TokenBalances balances={ tokensBalances } />
-                        </Col>
-                      </Row>
-                    </div>)}
           </CardText>
         </Card>);
     }
 }
 
-
-export default connect(
-  (state, ownProps) => ({
-    tokensBalances: tokens.selectors.balancesByAddress(state.tokens, ownProps.account.get('id')),
-  }),
-  (dispatch, ownProps) => ({
-
-  })
-)(Account);
+export default Account;
