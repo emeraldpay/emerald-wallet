a774ade651ce3c6f5101e68cee520ae206dcf79c
gagarin55 <gagarin55@protonmail.com>
2018-05-24 12:06:48 +0300

    problem: we should request addresses from ledger only after connection

--- src/components/ledger/ImportAccount/ImportAccount.js
+++ src/components/ledger/ImportAccount/ImportAccount.js
@@ -1,42 +1,32 @@
 // @flow
+import React from 'react';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import { Button, ButtonGroup } from 'emerald-js-ui';
+import { AddCircle as AddIcon } from 'emerald-js-ui/lib/icons3';
 import DashboardButton from 'components/common/DashboardButton';
 import HDPath from 'components/common/HdPath';
 import { Form, Row, styles as formStyles } from 'elements/Form';
-import { Button, ButtonGroup } from 'emerald-js-ui';
-import { AddCircle as AddIcon } from 'emerald-js-ui/lib/icons3';
-import { fromJS } from 'immutable';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-import React from 'react';
-import { connect } from 'react-redux';
-import ledger from 'store/ledger';
-import screen from 'store/wallet/screen';
-import Accounts from '../../../store/vault/accounts';
-import WaitDialog from '../WaitDialog';
-import styles from './ImportAccount.scss';
 import AddrList from './addrlist';
 import Pager from './pager';
 
+import styles from './ImportAccount.scss';
 
 type Props = {
-  init: Function,
+  onInit: Function,
   onBackScreen: ?Function,
-  connected: boolean,
   hdbase: string,
 }
 
 class ImportAccount extends React.Component<Props> {
   componentDidMount() {
-    if (this.props.init) {
-      this.props.init();
+    if (this.props.onInit) {
+      this.props.onInit();
     }
   }
 
   render() {
-    const { connected, hdbase, changeBaseHD, muiTheme, selected } = this.props;
+    const { hdbase, changeBaseHD, muiTheme, selected } = this.props;
     const { onAddSelected, onCancel, onDashboard } = this.props;
-    if (!connected) {
-      return (<WaitDialog />);
-    }
     return (
       <Form
         caption="Import Ledger hardware account"
@@ -77,41 +67,4 @@ class ImportAccount extends React.Component<Props> {
   }
 }
 
-export default connect(
-  (state, ownProps) => ({
-    hdbase: state.ledger.getIn(['hd', 'base']),
-    connected: state.ledger.get('connected'),
-    selected: state.ledger.get('selectedAddr') !== null,
-  }),
-  (dispatch, ownProps) => ({
-    changeBaseHD: (hdpath: string) => {
-      dispatch(ledger.actions.setBaseHD(hdpath));
-      dispatch(ledger.actions.getAddresses());
-    },
-    init: () => dispatch(ledger.actions.getAddresses()),
-    onDashboard: () => {
-      if (ownProps.onBackScreen) {
-        return dispatch(screen.actions.gotoScreen(ownProps.onBackScreen));
-      }
-      dispatch(screen.actions.gotoScreen('home'));
-    },
-    onAddSelected: () => {
-      let acc = null;
-      dispatch(ledger.actions.importSelected())
-        .then((address) => {
-          acc = fromJS({ id: address });
-          return dispatch(Accounts.actions.loadAccountsList());
-        })
-        .then(() => {
-          // go to account details only when accounts updated
-          return dispatch(screen.actions.gotoScreen('account', acc));
-        });
-    },
-    onCancel: () => {
-      if (ownProps.onBackScreen) {
-        return dispatch(screen.actions.gotoScreen(ownProps.onBackScreen));
-      }
-      dispatch(screen.actions.gotoScreen('home'));
-    },
-  })
-)(muiThemeable()(ImportAccount));
+export default muiThemeable()(ImportAccount);
---
+++ src/components/ledger/ImportAccount/ImportAccountContainer.js
@@ -0,0 +1,58 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import { fromJS } from 'immutable';
+import ledger from '../../../store/ledger';
+import screen from '../../../store/wallet/screen';
+import Accounts from '../../../store/vault/accounts';
+import ImportAccount from './ImportAccount';
+import WaitDialog from '../WaitDialog';
+
+const Container = (props) => {
+  const { connected, ...passProps } = props;
+  if (!connected) {
+    return (<WaitDialog />);
+  }
+  return (
+    <ImportAccount {...passProps} />
+  );
+};
+
+export default connect(
+  (state, ownProps) => ({
+    hdbase: state.ledger.getIn(['hd', 'base']),
+    connected: state.ledger.get('connected'),
+    selected: state.ledger.get('selectedAddr') !== null,
+  }),
+  (dispatch, ownProps) => ({
+    changeBaseHD: (hdpath: string) => {
+      dispatch(ledger.actions.setBaseHD(hdpath));
+      dispatch(ledger.actions.getAddresses());
+    },
+    onInit: () => dispatch(ledger.actions.getAddresses()),
+    onDashboard: () => {
+      if (ownProps.onBackScreen) {
+        return dispatch(screen.actions.gotoScreen(ownProps.onBackScreen));
+      }
+      dispatch(screen.actions.gotoScreen('home'));
+    },
+    onAddSelected: () => {
+      let acc = null;
+      dispatch(ledger.actions.importSelected())
+        .then((address) => {
+          acc = fromJS({ id: address });
+          return dispatch(Accounts.actions.loadAccountsList());
+        })
+        .then(() => {
+          // go to account details only when accounts updated
+          return dispatch(screen.actions.gotoScreen('account', acc));
+        });
+    },
+    onCancel: () => {
+      if (ownProps.onBackScreen) {
+        return dispatch(screen.actions.gotoScreen(ownProps.onBackScreen));
+      }
+      dispatch(screen.actions.gotoScreen('home'));
+    },
+  })
+)(Container);
