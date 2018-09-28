f28dd1518c2c73fd241bf7a50976b3fe3dc36f1c
BelfordZ <belfordz66@gmail.com>
2018-09-28 00:31:57 -0700

    remove sass from welcome

--- src/components/welcome/NodeTypeChoice/nodeTypeChoice.js
+++ src/components/welcome/NodeTypeChoice/nodeTypeChoice.js
@@ -10,19 +10,66 @@ import FullNodeLogo from './fullNodeLogo';
 import RemoteNodeLogo from './remoteNodeLogo';
 
 import classes from './nodeTypeChoice.scss';
+const styles = {
+  container: {
+    paddingTop: '40px',
+    paddingBottom: '50px',
+  },
+
+  optionsContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '49px',
+  },
+
+  nodeRectangle: {
+    display: 'flex',
+    border: '1px solid #DDDDDD',
+    boxSizing: 'border-box',
+    marginLeft: '14.5px',
+    marginRight: '14.5px',
+    paddingTop: '49px',
+    paddingLeft: '35px',
+    paddingRight: '35px',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    maxWidth: '300px',
+    maxHeight: '360px',
+  },
+
+  rectangleItem: {
+    marginBottom: '30px',
+  },
+
+  title: {
+    color: '#191919',
+    display: 'flex',
+    fontSize: '22px',
+    lineHeight: '24px',
+    justifyContent: 'center',
+  },
+
+  description: {
+    color: '#747474',
+    fontSize: '14px',
+    lineHeight: '20px',
+    textAlign: 'center',
+  },
+};
 
 const NodeTypeChoice = ({ useFullNode, useRemoteNode }) => {
   return (
     <Card>
-      <div className={ classes.container }>
-        <div className={ classes.title }>Select how you're going to connect to the network</div>
-        <div className={ classes.optionsContainer }>
-          <div className={ classes.nodeRectangle }>
-            <div className={ classes.rectangleItem }><FullNodeLogo /></div>
-            <div className={ cx(classes.rectangleItem, classes.description) }>
+      <div style={ styles.container }>
+        <div style={ styles.title }>Select how you're going to connect to the network</div>
+        <div style={ styles.optionsContainer }>
+          <div style={ styles.nodeRectangle }>
+            <div style={ styles.rectangleItem }><FullNodeLogo /></div>
+            <div style={ cx(styles.rectangleItem, styles.description) }>
                             More secure. But it takes a few hours to sync.
             </div>
-            <div className={ classes.rectangleItem }>
+            <div style={ styles.rectangleItem }>
               <Button
                 primary
                 label="Full Node"
@@ -30,12 +77,12 @@ const NodeTypeChoice = ({ useFullNode, useRemoteNode }) => {
               />
             </div>
           </div>
-          <div className={ classes.nodeRectangle }>
-            <div className={ classes.rectangleItem }><RemoteNodeLogo/></div>
-            <div className={ cx(classes.rectangleItem, classes.description) }>
+          <div style={ styles.nodeRectangle }>
+            <div style={ styles.rectangleItem }><RemoteNodeLogo/></div>
+            <div style={ cx(styles.rectangleItem, styles.description) }>
                             Less secure. No need for long sync.
             </div>
-            <div className={ classes.rectangleItem }>
+            <div style={ styles.rectangleItem }>
               <Button
                 label="Remote Node"
                 onClick={ useRemoteNode }
