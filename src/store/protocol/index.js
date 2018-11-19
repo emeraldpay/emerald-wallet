ab614e529f6d3d798216dccc6e552bec4f089d2f
Zachary Belford <belfordz66@gmail.com>
2018-11-19 08:08:45 -0800

    contract deploy

--- src/store/protocol/index.js
+++ src/store/protocol/index.js
@@ -29,6 +29,12 @@ const protocolLinkHandler = (request, state, dispatch) => {
     };
   }
 
+  const isContractConstructor = paymentParams.mode && paymentParams.mode === 'contract_constructor';
+  if (isContractConstructor) {
+    // do constructory things
+    contractCallArgs.data = paymentParams.data;
+  }
+
   const transaction = fromJS({
     amount: paymentParams.value,
     gas: paymentParams.gas,
