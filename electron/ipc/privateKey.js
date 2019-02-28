9353e28c83430165fab3b327e10c63a78b9da623
gagarin55 <gagarin55@protonmail.com>
2019-02-28 13:21:31 +0300

    fixed private key import

--- electron/ipc/privateKey.js
+++ electron/ipc/privateKey.js
@@ -1,15 +1,15 @@
 const { ipcMain } = require('electron'); // eslint-disable-line import/no-extraneous-dependencies
-const { Wallet } = require('@emeraldplatform/emerald-js');
+const { EthAccount } = require('@emeraldplatform/eth-account');
 
 module.exports = () => {
   ipcMain.on('get-private-key', (event, {keyfile, passphrase}) => {
-    const wallet = Wallet.fromV3(keyfile, passphrase);
+    const wallet = EthAccount.fromV3(keyfile, passphrase);
     const privateKey = wallet.getPrivateKeyString();
     event.sender.send('recieve-private-key', privateKey);
   });
 
   ipcMain.on('get-private-key-to-keyfile', (event, {privateKey, password}) => {
-    const keyfile = Wallet.fromPrivateKey(privateKey).toV3String(password);
+    const keyfile = EthAccount.fromPrivateKey(privateKey).toV3String(password);
     event.sender.send('recieve-private-key-to-keyfile', keyfile);
   });
 };
