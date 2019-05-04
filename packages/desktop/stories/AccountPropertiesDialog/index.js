4d185f8b4b9c8f6265d1b62326e44a43532591f8
gagarin55 <gagarin55@protonmail.com>
2019-05-04 06:18:44 +0300

    minor fix

--- packages/desktop/stories/AccountPropertiesDialog/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import AccountPropertiesDialog from '../../src/components/accounts/GenerateAccount/AccountPropertiesDialog';
-
-storiesOf('AccountPropertiesDialog', module)
-  .add('default', () => <AccountPropertiesDialog onSkip={action('onSkip')}/>);
