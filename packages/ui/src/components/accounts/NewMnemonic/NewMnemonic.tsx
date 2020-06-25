8c9b4a2b3a0d03ff8c7696a72dcb766ef6cd94e2
Igor Artamonov <igor@artamonov.ru>
2020-06-24 23:01:39 -0400

    solution: polish ui of the New Mnemonic component

--- packages/ui/src/components/accounts/NewMnemonic/NewMnemonic.tsx
+++ packages/ui/src/components/accounts/NewMnemonic/NewMnemonic.tsx
@@ -12,14 +12,35 @@ const useStyles = makeStyles(
       alignItems: 'center',
     },
     mnemonic: {
-      padding: "20px",
-      margin: "20px",
-      fontSize: "1.2em",
-      border: "1px solid #f0f0f0"
+      fontSize: "0.9em",
+      border: "1px solid #f0f0f0",
+      padding: "20px 20px",
+      margin: "10px 0px",
+      backgroundColor: "#f0faff",
+    },
+    mnemonicEmpty: {
+      textAlign: "center"
     },
     button: {
       width: "220px",
       margin: "5px"
+    },
+    wordIndex: {
+      display: "inline-block",
+      width: "24px",
+      opacity: "75%",
+      fontSize: "0.8em"
+    },
+    writeMessage: {
+      width: "600px",
+      float: "left",
+    },
+    writeButtons: {
+      width: "200px",
+      float: "left",
+    },
+    confirmButtons: {
+      padding: "16px"
     }
   })
 );
@@ -45,6 +66,7 @@ const Component = ((props: OwnProps) => {
   const [mnemonic, setMnemonic] = React.useState("");
   const [confirming, setConfirming] = React.useState(false);
   const [confirmation, setConfirmation] = React.useState("");
+  const [expectPassword, setExpectPassword] = React.useState(false);
   const [password, setPassword] = React.useState("");
   const [done, setDone] = React.useState(false);
 
@@ -53,98 +75,124 @@ const Component = ((props: OwnProps) => {
   const hasMnemonic = typeof mnemonic == 'string' && mnemonic.length > 0;
   const confirmed = hasMnemonic && confirmation.toLowerCase().replace(/\s+/g, " ").trim() == mnemonic.toLowerCase();
 
-  const warning = hasMnemonic ? <Box>
-    <Alert severity="error">
-      Please copy and keep this secret phrase ("Mnemonic Phrase") and password, if set, in a safe place.
-      Never tell it to anyone. Emerald representative would never ask for it.
-      If you lose this phrase, you will not be able to recover your account.
-    </Alert>
-  </Box> : <Box/>;
-
   let passwordField;
 
-  if (password && password.length > 0) {
-    passwordField = <Alert severity="success">
-      Phrase protection password is set.
-      <Button variant={"text"} disabled={done} onClick={() => setPassword("")}>Reset</Button>
-    </Alert>
+  if (expectPassword) {
+    if (password && password.length > 0) {
+      passwordField = <Alert severity="success">
+        Phrase protection password is set.
+        <Button variant={"text"} disabled={done} onClick={() => {
+          setPassword("");
+          setExpectPassword(false);
+        }}>Reset</Button>
+      </Alert>
+    } else {
+      passwordField =
+        <ConfirmedPasswordInput helperText={"(optional) Additional passphrase to protect the secret mnemonic phrase."}
+                                disabled={done}
+                                buttonLabel={"Set password"}
+                                onChange={setPassword}/>
+    }
   } else {
-    passwordField =
-      <ConfirmedPasswordInput helperText={"(optional) Additional password to protect the secret mnemonic phrase."}
-                              disabled={done}
-                              buttonLabel={"Set password"}
-                              onChange={setPassword}/>
+    passwordField = <Alert severity="info">
+      You may set an additional passphrase to your secret phrase.
+      <Button variant={"text"} disabled={done} onClick={() => setExpectPassword(true)}>Set Passphrase</Button>
+    </Alert>
   }
 
   let content;
   let title: string;
-  let subtitle: string;
 
   if (confirming) {
     title = "Confirm generated phrase";
-    subtitle = "Please enter the secret phrase, to confirm you have saved a copy of it.";
-    content = <Box>
-      <TextField
-        disabled={done}
-        fullWidth={true}
-        multiline={true}
-        rowsMax={4}
-        rows={4}
-        onChange={(e) => setConfirmation(e.target.value)}
-      />
-      {passwordField}
-    </Box>
+    content = <Grid container={true}>
+      <Grid item={true} xs={12}>
+        <TextField
+          placeholder={"Enter the secret phrase, only words without numbers"}
+          disabled={done}
+          fullWidth={true}
+          multiline={true}
+          rowsMax={4}
+          rows={4}
+          onChange={(e) => setConfirmation(e.target.value)}
+        />
+      </Grid>
+      <Grid item={true} xs={9}>
+        {passwordField}
+      </Grid>
+      <Grid item={true} xs={3} className={styles.confirmButtons}>
+        <Button color={"primary"}
+                disabled={!confirmed || done}
+                variant={"contained"}
+                onClick={() => {
+                  onContinue(mnemonic, password.length > 0 ? password : undefined);
+                  setDone(true);
+                }}>Confirm</Button>
+        <Button variant={"text"}
+                disabled={done}
+                onClick={() => setConfirming(false)}>Cancel</Button>
+      </Grid>
+    </Grid>
   } else {
-    // const mnemonicText = hasMnemonic ? mnemonic.split(" ")
-    //   .map((word, i) => i + ". " + word)
-    //   .join(" ") : "";
-    title = "Secret phrase";
-    subtitle = "Click `" + (hasMnemonic ? "Regenerate" : "Generate") + "` to create a new phrase."
-    content = <Typography variant={"body1"} className={styles.mnemonic}>
-      {mnemonic}
-    </Typography>
+    title = "Create secret \"mnemonic\" phrase";
+
+    if (hasMnemonic) {
+      const words = mnemonic.split(" ")
+        .map((word, i) =>
+          <Grid item={true} xs={3} key={"word-" + i}>
+            <Typography variant={"body2"}><span className={styles.wordIndex}>{i + 1}.</span> {word}</Typography>
+          </Grid>
+        )
+
+      const message = <Box>
+        <Alert severity="error">
+          <Box className={styles.writeMessage}>
+            Please write the phrase ("Mnemonic Phrase") and keep it in a safe place.
+            Never tell it to anyone.
+            Don't enter it anywhere online. It's the key to spend your funds.
+            If you lose this phrase, you will not be able to recover your account.
+          </Box>
+          <Box className={styles.writeButtons}>
+            <Button color={"primary"}
+                    disabled={done}
+                    className={styles.button}
+                    variant={"contained"}
+                    onClick={() => setConfirming(true)}>Yes, I wrote it down</Button>
+          </Box>
+        </Alert>
+      </Box>
+
+      content = <Grid container={true} className={styles.mnemonic}>
+        {words}
+        <Grid item={true} xs={8}/>
+        <Grid item={true} xs={4}>
+          <Button color={"primary"}
+                  className={styles.button}
+                  variant={"text"}
+                  onClick={() => onGenerate().then(setMnemonic).catch(console.error)}>
+            Generate New
+          </Button>
+        </Grid>
+        <Grid item={true} xs={12}>{message}</Grid>
+      </Grid>
+    } else {
+      content = <Box className={styles.mnemonic + " " + styles.mnemonicEmpty}>
+        <Typography>Click "Generate Phrase" to create a new secret phrase that will be a key to spend your
+          cryptocurrencies.</Typography>
+        <Button color={"primary"}
+                className={styles.button}
+                variant={"text"}
+                onClick={() => onGenerate().then(setMnemonic).catch(console.error)}>
+          Generate Phrase
+        </Button>
+      </Box>
+    }
   }
 
   return <Grid container={true}>
-    <Grid item={true} xs={8}>
-      <Grid container={true}>
-        <Grid item={true} xs={12}><Typography variant={"h4"}>{title}</Typography></Grid>
-        <Grid item={true} xs={12}><Typography variant={"subtitle1"}>{subtitle}</Typography></Grid>
-        <Grid item={true} xs={12}>{content}</Grid>
-        <Grid item={true} xs={12}>{warning}</Grid>
-      </Grid>
+    <Grid item={true} xs={12}><Typography variant={"h6"}>{title}</Typography></Grid>
+    <Grid item={true} xs={12}>{content}</Grid>
     </Grid>
-    <Grid item={true} xs={4} className={styles.controls}>
-      {hasMnemonic && !confirming &&
-      <Button color={"primary"}
-              disabled={done}
-              className={styles.button}
-              variant={"contained"}
-              onClick={() => setConfirming(true)}>Enter Confirmation</Button>}
-      {!confirming &&
-      <Button color={hasMnemonic ? "secondary" : "primary"}
-              className={styles.button}
-              variant={"contained"}
-              disabled={confirming || done}
-              onClick={() => onGenerate().then(setMnemonic).catch(console.error)}>
-        {hasMnemonic ? 'Regenerate' : 'Generate'}
-      </Button>}
-      {hasMnemonic && confirming &&
-      <Button color={"primary"}
-              className={styles.button}
-              disabled={!confirmed || done}
-              variant={"contained"}
-              onClick={() => {
-                onContinue(mnemonic, password.length > 0 ? password : undefined);
-                setDone(true);
-              }}>Confirm</Button>}
-      {confirming &&
-      <Button variant={"text"}
-              disabled={done}
-              className={styles.button}
-              onClick={() => setConfirming(false)}>Cancel</Button>}
-    </Grid>
-  </Grid>
 })
 
 export default Component;
