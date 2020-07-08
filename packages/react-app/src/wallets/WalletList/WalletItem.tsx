d88d33fbf81fd0a49c82f73ef0e6b557ff4a27fb
Igor Artamonov <igor@artamonov.ru>
2020-07-07 23:36:24 -0400

    solution: update design of the wallet card on the home screen

--- packages/react-app/src/wallets/WalletList/WalletItem.tsx
+++ packages/react-app/src/wallets/WalletList/WalletItem.tsx
@@ -1,25 +1,19 @@
 import {Blockchains, Units, Wallet} from '@emeraldwallet/core';
 import {
-  Box,
   Button,
   Card,
-  CardActions,
   CardContent,
-  CardHeader,
   Grid,
-  GridSize,
   Theme,
   Typography
 } from '@material-ui/core';
-import {createStyles, makeStyles, withStyles, withTheme} from '@material-ui/core/styles';
-import {AccountBalanceWalletOutlined as WalletIcon} from '@material-ui/icons';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
 import * as React from 'react';
 import {Balance} from "@emeraldwallet/ui";
 import {connect} from "react-redux";
 import {accounts, IBalanceValue, IState} from "@emeraldwallet/store";
 import {Dispatch} from "react";
-import {Block} from "@emeraldplatform/ui-icons"
-import {Wei} from "@emeraldplatform/eth";
+import {Hashicon} from "@emeraldpay/hashicon-react";
 
 const useStyles = makeStyles<Theme>((theme) =>
   createStyles({
@@ -28,62 +22,65 @@ const useStyles = makeStyles<Theme>((theme) =>
       padding: theme.spacing(),
       marginTop: theme.spacing(),
     },
-    gridCardInner: {
-      // @ts-ignore
-      backgroundColor: theme.emeraldColors.white.main,
-      margin: '10px',
-      width: '100%',
-      border: `1px solid ${theme.palette.divider}`,
-    },
     walletIcon: {
-      cursor: 'pointer'
+      cursor: 'pointer',
+      paddingLeft: "16px",
+      paddingTop: "24px",
     },
     headerAction: {
       width: "auto"
     },
     totalBalance: {
-      fontSize: "3.8em",
-      color: theme.palette.primary.main,
+      fontSize: "20px",
+      // @ts-ignore
+      color: theme.emeraldColors.coal,
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+      textOverflow: "ellipsis",
+      textAlign: "right",
+    },
+    title: {
+      // @ts-ignore
+      color: theme.emeraldColors.coal,
+      fontSize: "20px",
       overflow: "hidden",
       whiteSpace: "nowrap",
       textOverflow: "ellipsis",
     },
-    title: {
-      overflow: "hidden",
-      whiteSpace: "nowrap",
-      textOverflow: "ellipsis",
+    titleLine: {
+      paddingTop: "24px",
+      paddingBottom: "16px",
     },
     balance: {
       color: theme.palette.secondary.main,
-      textAlign: "right",
-    },
-    textIcon: {
       float: "left",
-      width: "34px"
+      paddingRight: "10px",
+    },
+    balanceRoot: {
+      display: "inline",
     },
     textBody: {
       width: "400px"
     },
     actions: {
+      textAlign: "center",
       clear: "both",
       marginTop: "20px",
       paddingLeft: theme.spacing(3)
     },
+    actionButton: {
+      width: "150px",
+      margin: "5px 10px",
+    }
   })
 );
-
-function balanceLength(value: Units | Wei): number {
-  if (Units.isUnits(value)) {
-    return value.toString().length + 3;
-  }
-  return value.toString(undefined, 2, false, true).length
-}
 
 /**
  *
  */
 const Component = (({wallet, openWallet, assets, total}: Props & Actions & OwnProps) => {
   const styles = useStyles();
+  const [current, setCurrent] = React.useState(false)
 
   function handleDetailsClick() {
     if (openWallet) {
@@ -92,56 +89,51 @@ const Component = (({wallet, openWallet, assets, total}: Props & Actions & OwnPr
   }
 
   return (
-    <Card variant={"outlined"} className={styles.root}>
+    <Card variant={"elevation"}
+          elevation={current ? 3 : 0}
+          className={styles.root}
+          onMouseOver={() => setCurrent(true)}
+          onMouseOut={() => setCurrent(false)}>
       <CardContent>
-        <Grid
-          item={true}
-          xs={12}
-        >
-          <Grid container={true}>
-            <Grid item={true} xs={6}>
-              <Typography variant={"h4"} className={styles.title}>
-                <WalletIcon color='secondary'/>
-                {wallet.name}
-              </Typography>
-              <Box>
-                <Block className={styles.textIcon}/>
-                <Typography className={styles.textBody}>
-                  {wallet.accounts.map((acc) =>
-                    Blockchains[acc.blockchain].getTitle()
-                  ).join(", ")}
-                </Typography>
-              </Box>
-              <Box className={styles.actions}>
-                <Button variant={"outlined"}
-                        onClick={handleDetailsClick}
-                        color={"primary"}>Wallet Details</Button>
-              </Box>
-            </Grid>
-            <Grid item={true} xs={6}>
-              <Grid container={true}>
-                <Grid item={true} xs={12}>
-                  {total && <Balance classes={{coins: styles.totalBalance}}
-                                     balance={total.balance}
-                                     symbol={total.token}
-                                     displayDecimals={2}/>}
-                </Grid>
-                {assets.map((asset) => {
-                  const l = balanceLength(asset.balance);
-                  let xs = 3;
-                  if (l > 6) xs = xs + 1;
-                  return (
-                    <Grid item={true} xs={xs as GridSize} key={asset.token}>
-                      <Balance balance={asset.balance} symbol={asset.token} displayDecimals={2}
-                               classes={{coins: styles.balance}}/>
-                    </Grid>
-                  );
-                })}
+        <Grid container={true}>
+          <Grid item={true} xs={2} className={styles.walletIcon} onClick={handleDetailsClick}>
+            <Hashicon value={"WALLET/" + wallet.id} size={100}/>
+          </Grid>
+          <Grid item={true} xs={7} onClick={handleDetailsClick}>
+            <Grid container={true}>
+              <Grid item={true} xs={8} className={styles.titleLine}>
+                <Typography className={styles.title}>{wallet.name}</Typography>
+              </Grid>
+              <Grid item={true} xs={4} className={styles.titleLine}>
+                {total && <Balance classes={{coins: styles.totalBalance}}
+                                   balance={total.balance}
+                                   symbol={total.token}
+                                   displayDecimals={2}/>}
+              </Grid>
+              <Grid item={true} xs={12}>
+                {assets.map((asset) => (
+                    <Balance balance={asset.balance} symbol={asset.token} displayDecimals={2}
+                             classes={{coins: styles.balance, root: styles.balanceRoot}}/>
+                  )
+                )}
               </Grid>
             </Grid>
           </Grid>
+          <Grid item={true} xs={3} className={styles.actions}>
+            <Button variant={"contained"}
+                    onClick={() => {
+                    }}
+                    className={styles.actionButton}
+                    color={"secondary"}>Send</Button>
+            <Button variant={"contained"}
+                    onClick={() => {
+                    }}
+                    className={styles.actionButton}
+                    color={"secondary"}>Receive</Button>
+          </Grid>
         </Grid>
       </CardContent>
+
     </Card>
 
   )
