3ce298d52b0a91f977a0121a118bc69a34ab77d1
Igor Artamonov <igor@artamonov.ru>
2020-10-03 15:04:40 -0400

    problem: wrong balance and duplicate utxo after update

--- packages/store/src/accounts/reducer.spec.ts
+++ packages/store/src/accounts/reducer.spec.ts
@@ -77,4 +77,130 @@ describe('accounts reducer', () => {
     expect(state.wallets[1].id).toEqual('2d9fde4e-ce00-4b58-af68-15c211604529');
   });
 
+  it('SET_BALANCE updates utxo', () => {
+    let state: IAccountsState = reducer(undefined,
+      {
+        type: ActionTypes.SET_BALANCE,
+        payload: {
+          entryId: "c35d05ba-d6bb-40b1-9553-383f414a97e5-1",
+          value: "",
+          utxo: [
+            {
+              txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+              vout: 0,
+              value: "100/SAT",
+              address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+            }
+          ]
+        }
+      }
+    );
+
+    expect(state.details[0].utxo).toEqual([
+      {
+        txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+        vout: 0,
+        value: "100/SAT",
+        address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+      }
+    ]);
+  });
+
+  it('SET_BALANCE doesnt duplicate utxo', () => {
+    let state: IAccountsState = reducer(undefined,
+      {
+        type: ActionTypes.SET_BALANCE,
+        payload: {
+          entryId: "c35d05ba-d6bb-40b1-9553-383f414a97e5-1",
+          value: "",
+          utxo: [
+            {
+              txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+              vout: 0,
+              value: "100/SAT",
+              address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+            }
+          ]
+        }
+      }
+    );
+    state = reducer(state,
+      {
+        type: ActionTypes.SET_BALANCE,
+        payload: {
+          entryId: "c35d05ba-d6bb-40b1-9553-383f414a97e5-1",
+          value: "",
+          utxo: [
+            {
+              txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+              vout: 0,
+              value: "100/SAT",
+              address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+            }
+          ]
+        }
+      }
+    );
+
+    expect(state.details[0].utxo).toEqual([
+      {
+        txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+        vout: 0,
+        value: "100/SAT",
+        address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+      }
+    ]);
+  });
+
+  it('SET_BALANCE add new utxo', () => {
+    let state: IAccountsState = reducer(undefined,
+      {
+        type: ActionTypes.SET_BALANCE,
+        payload: {
+          entryId: "c35d05ba-d6bb-40b1-9553-383f414a97e5-1",
+          value: "",
+          utxo: [
+            {
+              txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+              vout: 0,
+              value: "100/SAT",
+              address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+            }
+          ]
+        }
+      }
+    );
+    state = reducer(state,
+      {
+        type: ActionTypes.SET_BALANCE,
+        payload: {
+          entryId: "c35d05ba-d6bb-40b1-9553-383f414a97e5-1",
+          value: "",
+          utxo: [
+            {
+              txid: "6ef31eced444201545e845410b04af0737c0d41a0d6b50f6ffb5e04ace662208",
+              vout: 0,
+              value: "200/SAT",
+              address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+            }
+          ]
+        }
+      }
+    );
+
+    expect(state.details[0].utxo).toEqual([
+      {
+        txid: "0d41a0d6b50f6ffb5e04ace6622086ef31eced444201545e845410b04af0737c",
+        vout: 0,
+        value: "100/SAT",
+        address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+      },
+      {
+        txid: "6ef31eced444201545e845410b04af0737c0d41a0d6b50f6ffb5e04ace662208",
+        vout: 0,
+        value: "200/SAT",
+        address: "tb1qepcagv9wkp04ygq3ud33qrkk6482ulhkegc333"
+      }
+    ]);
+  });
 });
