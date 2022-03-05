ed340d1d672cce67c82c4d94cf0bd69ab096c15c
Sergei Novikov <boomer.create@gmail.com>
2022-03-05 12:59:31 +0300

    Balance format edited

--- packages/react-app/src/transaction/CreateTx/TokenField/TokenField.tsx
+++ packages/react-app/src/transaction/CreateTx/TokenField/TokenField.tsx
@@ -1,4 +1,5 @@
-import { BigAmount, FormatterBuilder, Predicates } from "@emeraldpay/bigamount";
+import { BigAmount, FormatterBuilder, Predicates } from '@emeraldpay/bigamount';
+import { Satoshi, Wei, WEIS, WEIS_ETC } from '@emeraldpay/bigamount-crypto';
 import { createStyles, MenuItem, TextField } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 import * as React from 'react';
@@ -15,18 +16,8 @@ interface Props {
   onChangeToken?: any;
 }
 
-const balanceFormatter = new FormatterBuilder()
-  .when(Predicates.ZERO, (whenTrue: FormatterBuilder, whenFalse: FormatterBuilder): void => {
-    whenTrue.useTopUnit();
-    whenFalse.useOptimalUnit();
-  })
-  .number(3, true)
-  .append(' ')
-  .unitCode()
-  .build();
-
-const styles = (theme?: any): Record<string, any> => createStyles(
-  {
+const styles = (theme?: any): Record<string, any> =>
+  createStyles({
     balance: {
       color: theme.palette && theme.palette.text.secondary,
       wordSpacing: '3px',
@@ -34,15 +25,34 @@ const styles = (theme?: any): Record<string, any> => createStyles(
       fontWeight: 200,
       paddingLeft: '20px',
     },
-  },
-)
+  });
 
 export class TokenField extends React.Component<Props> {
   public onChangeToken = (event: any): void => {
     if (this.props.onChangeToken) {
       this.props.onChangeToken(event.target.value);
     }
-  }
+  };
+
+  public formatBalance = (balance: BigAmount): string => {
+    const units = Satoshi.is(balance)
+      ? [WEIS_ETC.units[0], WEIS_ETC.units[3], WEIS_ETC.units[5], WEIS_ETC.units[6]]
+      : Wei.is(balance)
+      ? [WEIS.units[0], WEIS.units[3], WEIS.units[5], WEIS.units[6]]
+      : undefined;
+
+    const balanceFormatter = new FormatterBuilder()
+      .when(Predicates.ZERO, (whenTrue, whenFalse): void => {
+        whenTrue.useTopUnit();
+        whenFalse.useOptimalUnit(undefined, units, 3);
+      })
+      .number(3, true)
+      .append(' ')
+      .unitCode()
+      .build();
+
+    return balanceFormatter.format(balance);
+  };
 
   public render(): ReactElement {
     const { classes, selectedToken, balance } = this.props;
@@ -52,17 +62,15 @@ export class TokenField extends React.Component<Props> {
     return (
       <React.Fragment>
         <FormLabel>Token</FormLabel>
-        <TextField
-          select={true}
-          value={selectedToken}
-          onChange={this.onChangeToken}
-        >
+        <TextField select={true} value={selectedToken} onChange={this.onChangeToken}>
           {tokenSymbols.map((symbol) => (
-            <MenuItem key={symbol} value={symbol}>{symbol}</MenuItem>
+            <MenuItem key={symbol} value={symbol}>
+              {symbol}
+            </MenuItem>
           ))}
         </TextField>
         <div className={classes.balance} data-testid="balance">
-          {balance == null ? '?' : balanceFormatter.format(balance)}
+          {balance == null ? '?' : this.formatBalance(balance)}
           {` / ${this.props.fiatBalance} ${this.props.fiatCurrency}`}
         </div>
       </React.Fragment>
--- packages/ui/src/components/accounts/Balance/Balance.tsx
+++ packages/ui/src/components/accounts/Balance/Balance.tsx
@@ -1,6 +1,7 @@
-import { BigAmount, FormatterBuilder, Predicates } from "@emeraldpay/bigamount";
-import { createStyles, IconButton, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { BigAmount, FormatterBuilder, Predicates, Unit } from '@emeraldpay/bigamount';
+import { Satoshi, Wei, WEIS, WEIS_ETC } from '@emeraldpay/bigamount-crypto';
+import { createStyles, IconButton, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Sync } from '@material-ui/icons';
 import { ClassNameMap } from '@material-ui/styles';
 import * as React from 'react';
@@ -27,24 +28,30 @@ export interface OwnProps {
   onConvert?: () => void;
 }
 
-const formatSelector = (whenTrue: FormatterBuilder, whenFalse: FormatterBuilder): void => {
-  whenTrue.useTopUnit();
-  whenFalse.useOptimalUnit();
-};
-
-const coinFormatter = new FormatterBuilder()
-  .when(Predicates.ZERO, formatSelector)
-  .number(3, true)
-  .build();
-const unitFormatter = new FormatterBuilder()
-  .when(Predicates.ZERO, formatSelector)
-  .unitCode()
-  .build();
-
 const useStyles = makeStyles(styles);
 
 const Component: React.FC<OwnProps> = ({ balance, classes = {}, onConvert }) => {
   const styles = useStyles();
+
+  const formatBalance = React.useCallback((balance: BigAmount) => {
+    const units = Satoshi.is(balance)
+      ? [WEIS_ETC.units[0], WEIS_ETC.units[3], WEIS_ETC.units[5], WEIS_ETC.units[6]]
+      : Wei.is(balance)
+      ? [WEIS.units[0], WEIS.units[3], WEIS.units[5], WEIS.units[6]]
+      : undefined;
+
+    const formatSelector = (whenTrue: FormatterBuilder, whenFalse: FormatterBuilder): void => {
+      whenTrue.useTopUnit();
+      whenFalse.useOptimalUnit(undefined, units, 3);
+    };
+
+    const coinFormatter = new FormatterBuilder().when(Predicates.ZERO, formatSelector).number(3, true).build();
+    const unitFormatter = new FormatterBuilder().when(Predicates.ZERO, formatSelector).unitCode().build();
+
+    return [coinFormatter.format(balance), unitFormatter.format(balance)];
+  }, []);
+
+  const [coinBalance, balanceUnit] = formatBalance(balance);
 
   return (
     <div className={`${styles.root} ${classes?.root}`}>
@@ -54,15 +61,11 @@ const Component: React.FC<OwnProps> = ({ balance, classes = {}, onConvert }) => 
         </IconButton>
       )}
       <Typography className={`${styles.coins} ${classes?.coins}`}>
-        <span className={`${styles.coinBalance} ${classes?.coinBalance}`}>
-          {balance == null ? '-' : coinFormatter.format(balance)}
-        </span>
-        <span className={`${styles.coinSymbol} ${classes?.coinSymbol}`}>
-          {balance == null ? '' : unitFormatter.format(balance)}
-        </span>
+        <span className={`${styles.coinBalance} ${classes?.coinBalance}`}>{balance == null ? '-' : coinBalance}</span>
+        <span className={`${styles.coinSymbol} ${classes?.coinSymbol}`}>{balance == null ? '' : balanceUnit}</span>
       </Typography>
     </div>
   );
-}
+};
 
 export default Component;
