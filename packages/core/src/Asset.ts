8d36b2e1d33ba975d26ffd693ab3fa9152664ec6
Sergei Novikov <boomer.create@gmail.com>
2022-03-07 04:45:01 +0300

    problem: awkward amount formatting for small values
    Co-authored-by: Igor Artamonov <igor@artamonov.ru>

--- packages/core/src/Asset.ts
+++ packages/core/src/Asset.ts
@@ -1,4 +1,6 @@
-import { CoinTickerCode } from './blockchains';
+import {CoinTickerCode} from './blockchains';
+import {BigAmount, Unit} from "@emeraldpay/bigamount";
+import {Satoshi, SATOSHIS, Wei, WeiEtc, WEIS, WEIS_ETC} from "@emeraldpay/bigamount-crypto";
 
 export type StableCoinCode = 'DAI' | 'USDC' | 'USDT';
 export type SupportedTokenCode = 'WEENUS' | 'WETH';
@@ -33,4 +35,20 @@ export const AssetDetails: Record<AnyCoinCode, AssetDetail> = {
   'USDT': { title: 'Tether' },
   'WETH': { title: 'Wrapped Ether' },
   'WEENUS': { title: 'Weenus' },
+}
+
+/**
+ * Get list of units that are supposed to be used to format the amount.
+ * @param amount
+ */
+export function getStandardUnits(amount: BigAmount): Unit[] | undefined {
+  if (Satoshi.is(amount)) {
+    return [SATOSHIS.units[0], SATOSHIS.units[3], SATOSHIS.units[4]];
+  } else if (WeiEtc.is(amount)) {
+    return [WEIS_ETC.units[0], WEIS_ETC.units[3], WEIS_ETC.units[5], WEIS_ETC.units[6]];
+  } else if (Wei.is(amount)) {
+    return [WEIS.units[0], WEIS.units[3], WEIS.units[5], WEIS.units[6]];
+  } else {
+    return undefined;
+  }
 }
