633d32544e6155a94821d2246447723978df168b
Sergei Novikov <boomer.create@gmail.com>
2022-10-14 04:34:24 +0300

    solution: icons for DAI and USDC

--- packages/ui/src/icons/index.ts
+++ packages/ui/src/icons/index.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 export {default as AddCircle} from './AddCircle';
 export {default as Add} from './Add';
 export {default as ArrowDown} from './ArrowDown';
@@ -27,9 +28,11 @@ export {default as Checkmark} from './Checkmark';
 export {default as Close} from './Close';
 export {default as Copytoclipboard} from './Copytoclipboard';
 export {default as CurrencyBtc} from './CurrencyBtc';
+export {default as CurrencyDai} from './CurrencyDai';
 export {default as CurrencyEtc} from './CurrencyEtc';
 export {default as CurrencyEth} from './CurrencyEth';
 export {default as CurrencyUsd} from './CurrencyUsd';
+export {default as CurrencyUsdc} from './CurrencyUsdc';
 export {default as CurrencyUsdt} from './CurrencyUsdt';
 export {default as Download} from './Download';
 export {default as EmoteHappy} from './EmoteHappy';
