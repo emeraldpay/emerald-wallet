f492eb175316776c34bfe8aa2077cf118e58dfd0
Sergei Novikov <boomer.create@gmail.com>
2022-10-19 15:32:24 +0300

    solution: fix tests

--- packages/react-app/src/transaction/CreateTx/TokenField/TokenField.spec.tsx
+++ packages/react-app/src/transaction/CreateTx/TokenField/TokenField.spec.tsx
@@ -1,7 +1,6 @@
 // tslint:disable-next-line:no-submodule-imports
-
 import '@testing-library/jest-dom/extend-expect';
-import { Wei } from '@emeraldpay/bigamount-crypto';
+import { WeiEtc } from '@emeraldpay/bigamount-crypto';
 import { Theme } from '@emeraldwallet/ui';
 import { ThemeProvider } from '@material-ui/core';
 import { render } from '@testing-library/react';
@@ -9,26 +8,48 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 import { TokenField } from './TokenField';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const reduceClasses = <T,>(prev: T, curr: any): T => ({ ...prev, [curr]: curr });
 const classes = Object.keys({ container: {} }).reduce(reduceClasses, {});
 
 describe('TokenField', () => {
   it('renders without crash', () => {
-    const wrapper = shallow(<TokenField classes={classes} selectedToken="ETC" tokenSymbols={['ETC']} />);
+    const wrapper = shallow(
+      <TokenField
+        classes={classes}
+        selectedToken="ETC"
+        tokenSymbols={['ETC']}
+        getBalanceByToken={() => new WeiEtc(1350000)}
+      />,
+    );
 
     expect(wrapper).toBeDefined();
   });
 
   it('should not crash without onChange handler', () => {
-    const wrapper = shallow<TokenField>(<TokenField classes={classes} selectedToken="ETC" tokenSymbols={['ETC']} />);
+    const wrapper = shallow<TokenField>(
+      <TokenField
+        classes={classes}
+        selectedToken="ETC"
+        tokenSymbols={['ETC']}
+        getBalanceByToken={() => new WeiEtc(350000)}
+      />,
+    );
 
-    wrapper.instance().onChangeToken({ target: { value: 'ETC' } });
+    wrapper.instance().onChangeToken({ target: { value: 'ETC' } } as React.ChangeEvent<HTMLInputElement>);
   });
 
   it('should render total balance', async () => {
     const component = render(
       <ThemeProvider theme={Theme}>
-        <TokenField classes={classes} selectedToken="ETC" tokenSymbols={[]} balance={new Wei(350000)} />,
+        <TokenField
+          classes={classes}
+          balance={new WeiEtc(350000)}
+          selectedToken="ETC"
+          tokenSymbols={[]}
+          getBalanceByToken={() => new WeiEtc(350000)}
+        />
+        ,
       </ThemeProvider>,
     );
 
