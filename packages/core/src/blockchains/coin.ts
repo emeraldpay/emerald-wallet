d11d56a345741dc4f8aca792cf8960ff0be316e5
Sergei Novikov <boomer.create@gmail.com>
2023-06-09 13:07:01 +0300

    problem: incorrect asset used to request and processing balance

--- packages/core/src/blockchains/coin.ts
+++ packages/core/src/blockchains/coin.ts
@@ -1,6 +1,24 @@
+import { BlockchainCode } from './blockchains';
+import { CoinTicker } from './coinTicker';
+
 export enum Coin {
   BTC = 'BTC',
   ETHER = 'ETHER',
 }
 
 export type CoinCode = 'BTC' | 'ETHER';
+
+export function getCoinAsset(asset: string, blockchain: BlockchainCode): string {
+  if (asset === Coin.ETHER) {
+    switch (blockchain) {
+      case BlockchainCode.ETC:
+        return CoinTicker.ETC;
+      case BlockchainCode.ETH:
+        return CoinTicker.ETH;
+      case BlockchainCode.Goerli:
+        return CoinTicker.ETG;
+    }
+  }
+
+  return asset;
+}
