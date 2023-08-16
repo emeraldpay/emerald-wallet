65143a3777203a54ba76b904fbc25a135e9f06d1
Sergei Novikov <boomer.create@gmail.com>
2023-08-16 16:34:49 +0300

    solution: prepare release v2.10.0

--- packages/ui/src/components/common/PasswordInput/PasswordInput.tsx
+++ packages/ui/src/components/common/PasswordInput/PasswordInput.tsx
@@ -35,6 +35,7 @@ const PasswordInput: React.FC<OwnProps> = ({
     <IconButton
       color={showPassword ? 'primary' : undefined}
       size="small"
+      tabIndex={-1}
       onClick={() => setShowPassword(!showPassword)}
     >
       <ShowPasswordIcon fontSize="small" />
